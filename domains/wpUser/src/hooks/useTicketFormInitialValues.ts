16e40c846d3566b96e9b9f3f172665e9f49e325d
Brent Christensen <brent@eventespresso.com>
2021-08-16 16:39:17 -0700

    hacka hacka hacka

--- domains/wpUser/src/hooks/useTicketFormInitialValues.ts
+++ domains/wpUser/src/hooks/useTicketFormInitialValues.ts
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { hooks, useTicketsMeta, Filters } from '@eventespresso/edtr-services';
 import { getOptionValues } from '@eventespresso/utils';
 import { useMemoStringify } from '@eventespresso/hooks';
+import { getEEDomData } from '@eventespresso/services';
 
 import { NAMESPACE } from '../constants';
 import useCapabilityOptions from './useCapabilityOptions';
@@ -22,9 +23,20 @@ const useTicketFormInitialValues = (): void => {
 		hooks.removeFilter(filterName, NAMESPACE);
 
 		hooks.addFilter(filterName, NAMESPACE, (initialValues, ticket) => {
+			const ticketID = ticket?.id;
+			console.log('%c ticketID', 'color: cyan;', ticketID);
 			// ticket the value from meta. It will be empty for new tickets
-			let capabilityRequired = getMetaValue<string>(ticket?.id, 'capabilityRequired', 'none');
+			let capabilityRequired = getMetaValue<string>(ticketID, 'capabilityRequired', 'none');
 			let customCapabilityRequired = '';
+
+			console.log('%c capabilityRequired', 'color: HotPink;', capabilityRequired);
+			console.log('%c customCapabilityRequired', 'color: HotPink;', customCapabilityRequired);
+
+			const ticketMeta = getEEDomData('wpUserData')?.ticketMeta;
+			console.log('%c ticketMeta', 'color: LimeGreen;', ticketMeta);
+			capabilityRequired = ticketMeta?.[ticketID]?.capabilityRequired || capabilityRequired;
+
+			console.log('%c capabilityRequired', 'color: HotPink;', capabilityRequired);
 
 			// if capabilityRequired has some unknown value
 			// it means, custom option should be selected
@@ -32,6 +44,9 @@ const useTicketFormInitialValues = (): void => {
 				customCapabilityRequired = `${capabilityRequired}`;
 				capabilityRequired = 'custom';
 			}
+
+			console.log('%c capabilityRequired', 'color: HotPink;', capabilityRequired);
+			console.log('%c customCapabilityRequired', 'color: HotPink;', customCapabilityRequired);
 
 			return {
 				...initialValues,
