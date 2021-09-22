01fdcb9f88323cc0418320fd8b52c709f79e2caa
Marlon Hario <hariomarlon83@gmail.com>
2021-09-23 06:03:23 +0800

    Fix lint.

--- packages/ui-components/src/InlineNotification/InlineNotification.tsx
+++ packages/ui-components/src/InlineNotification/InlineNotification.tsx
@@ -6,16 +6,20 @@ import type { InlineMessageProps } from './types';
 
 import './style.scss';
 
-export const InlineNotification: React.FC<InlineMessageProps> = ({ icon, id, message, type, variant, wrapperClassName }) => {
+export const InlineNotification: React.FC<InlineMessageProps> = ({ 
+	icon, 
+	id, 
+	message, 
+	type, 
+	variant, 
+	wrapperClassName 
+}) => {
 	const className = classNames(
 		'ee-inline-notification',
 		type && `ee-inline-notification--type-${type}`,
 		variant && `ee-inline-notification--variant-${variant}`
 	);
-	const wrapperClass = classNames(
-		'ee-inline-notification__wrapper',
-		wrapperClassName
-	);
+	const wrapperClass = classNames('ee-inline-notification__wrapper', wrapperClassName);
 
 	return (
 		<Collapsible show={Boolean(message?.length)} className={wrapperClass}>
