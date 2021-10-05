6582ffad02477037f07275d7b94d5667841d50af
Brent Christensen <brent@eventespresso.com>
2021-10-05 15:05:51 -0700

    remove unnecessary div

--- packages/ui-components/src/Grid/GridItem.tsx
+++ packages/ui-components/src/Grid/GridItem.tsx
@@ -5,9 +5,5 @@ import type { GridItemProps } from './types';
 export const GridItem: React.FC<GridItemProps> = ({ children, size, ...props }) => {
 	const className = classNames('ee-grid__item', size && `ee-grid__item--size-${size}`, props.className);
 
-	return (
-		<div>
-			<div className={className}>{children}</div>
-		</div>
-	);
+	return <div className={className}>{children}</div>;
 };
