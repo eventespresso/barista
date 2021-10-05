bc1f1258f1e5ae726f1a9c04d333227cce169bcc
Brent Christensen <brent@eventespresso.com>
2021-10-05 15:05:38 -0700

    make GridItem the wrapper :facepalm:

--- packages/ui-components/src/Grid/GridCard.tsx
+++ packages/ui-components/src/Grid/GridCard.tsx
@@ -4,19 +4,17 @@ import { Heading } from '../Heading';
 import { GridItem } from './GridItem';
 import type { GridCardProps } from './types';
 
-export const GridCard: React.FC<GridCardProps> = ({ children, size, header, ...props }) => {
-	const className = classNames('ee-grid-card', size && `ee-grid__item--size-${size}`, props.className);
+export const GridCard: React.FC<GridCardProps> = ({ children, header, ...props }) => {
+	const className = classNames('ee-grid-card__wrapper', props.className);
 
 	return (
-		<div>
+		<GridItem {...props} className={className}>
 			{header && (
 				<Heading as='h3' className='ee-grid-card__heading'>
 					{header}
 				</Heading>
 			)}
-			<GridItem size={size} className={className}>
-				<>{children}</>
-			</GridItem>
-		</div>
+			<div className='ee-grid-card'>{children}</div>
+		</GridItem>
 	);
 };
