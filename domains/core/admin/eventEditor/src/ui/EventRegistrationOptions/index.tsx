1ad9648c4458598a1a90fefa7b5a62f3d7161485
Brent Christensen <brent@eventespresso.com>
2021-10-07 16:52:09 -0700

    just load components instead of prop drilling

--- domains/core/admin/eventEditor/src/ui/EventRegistrationOptions/index.tsx
+++ domains/core/admin/eventEditor/src/ui/EventRegistrationOptions/index.tsx
@@ -1,9 +1,16 @@
-import { Grid } from '@eventespresso/ui-components';
+import { __ } from '@eventespresso/i18n';
+import { Grid, GridCard } from '@eventespresso/ui-components';
 import { noop } from '@eventespresso/utils';
 import { withFeature } from '@eventespresso/services';
 
-import RegistrationOptions from './RegistrationOptions';
-import EventDetails from './EventDetails';
+import ActiveStatus from './ActiveStatus';
+import AltRegPage from './AltRegPage';
+import DefaultRegistrationStatus from './DefaultRegistrationStatus';
+import Donations from './Donations';
+import EventManager from './EventManager';
+import EventPhoneNumber from './EventPhoneNumber';
+import MaxRegistrations from './MaxRegistrations';
+import TicketSelector from './TicketSelector';
 import withData from './withData';
 
 import type { EventRegistrationOptionsProps } from './types';
@@ -29,33 +36,26 @@ export const EventRegistrationOptions: React.FC<Partial<EventRegistrationOptions
 	phoneNumber,
 	status,
 }) => {
-	const eventDetails = {
-		allowDonations,
-		eventManagers,
-		managerId,
-		onDonationsChange,
-		onManagerChange,
-		onPhoneNumberChange,
-		onStatusChange,
-		phoneNumber,
-		status,
-	};
-
-	const registrationOptions = {
-		altRegPage,
-		defaultRegStatus,
-		displayTicketSelector,
-		maxReg,
-		onAltRegPageChange,
-		onDefaultRegStatusChange,
-		onTicketSelectorChange,
-		onMaxRegChange,
-	};
-
 	return (
-		<Grid className='ee-edtr-section ee-edtr-cards' maxColumns={2} size='big'>
-			<EventDetails {...eventDetails} />
-			<RegistrationOptions {...registrationOptions} />
+		<Grid className='ee-edtr-section ee-edtr-cards' maxColumns={2} size='bigger'>
+			<GridCard className='ee-event-details' header={__('Event Details')}>
+				<ActiveStatus status={status} onStatusChange={onStatusChange} />
+				<EventManager eventManagers={eventManagers} managerId={managerId} onManagerChange={onManagerChange} />
+				<EventPhoneNumber phoneNumber={phoneNumber} onPhoneNumberChange={onPhoneNumberChange} />
+				<Donations allowDonations={allowDonations} onDonationsChange={onDonationsChange} />
+			</GridCard>
+			<GridCard className='ee-reg-options' header={__('Registration Options')}>
+				<DefaultRegistrationStatus
+					defaultRegStatus={defaultRegStatus}
+					onDefaultRegStatusChange={onDefaultRegStatusChange}
+				/>
+				<TicketSelector
+					displayTicketSelector={displayTicketSelector}
+					onTicketSelectorChange={onTicketSelectorChange}
+				/>
+				<MaxRegistrations maxReg={maxReg} onMaxRegChange={onMaxRegChange} />
+				<AltRegPage altRegPage={altRegPage} onAltRegPageChange={onAltRegPageChange} />
+			</GridCard>
 		</Grid>
 	);
 };
