54625e91e8c3d522cb0a2d600720f74ace97cc16
Marlon Hario <hariomarlon83@gmail.com>
2021-11-04 19:56:41 +0800

    Break the loop into separate tests.

--- packages/e2e-tests/utils/admin/events/list/searchEvent.ts
+++ packages/e2e-tests/utils/admin/events/list/searchEvent.ts
@@ -1,6 +1,7 @@
-export const searchEvent = async (searchText = '', index: number) => {
+export const searchEvent = async (searchText: null | undefined | string, expected: boolean) => {
+	let returnValue: boolean;
 	// fill searchText into search field
-	await page.fill('input#toplevel_page_espresso_events-search-input', searchText);
+	await page.fill('input#toplevel_page_espresso_events-search-input', searchText ? searchText : '');
 
 	// trigger the search event button
 	await Promise.all([page.waitForNavigation(), page.click('input:has-text("Search Events")')]);
@@ -9,27 +10,13 @@ export const searchEvent = async (searchText = '', index: number) => {
 	const tableRows = await page.$$('table.wp-list-table tbody#the-list tr');
 
 	// Loop rows inside the tableRows result
-	if (tableRows.length && index !== 4) {
+	if (tableRows.length && expected) {
 		// Fetch event name in a row
 		const searchResult = await (await tableRows[0].$('td.column-name a.row-title')).innerText();
-
-		// Add some conditional to meet the specific assertions
-		if (index === 0) {
-			// Index 0 is always to the search result that's why it used toBe assertion
-			expect(searchResult.trim()).toBe(searchText.trim());
-		} else {
-			// Other index is always not equal to the search result but not empty so it used toBeTruthy assertion
-			expect(searchText).toBeTruthy();
-		}
+		returnValue = searchResult.trim() ? true : false;
 	} else {
-		// Add some conditional to meet the specific assertions
-		// Index 4 uses random text and null/empty value for search cases so in here it used toBeTruthy and toBeFalsy
-		if (searchText !== '') {
-			expect(searchText).toBeTruthy();
-		} else {
-			expect(searchText).toBeFalsy();
-		}
+		returnValue = false;
 	}
 
-	return tableRows.length;
+	return returnValue;
 };
