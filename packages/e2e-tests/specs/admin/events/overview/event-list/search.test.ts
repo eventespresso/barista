c1e5e378cace18f75f7dfdd94d5ae68c36c701e7
Marlon Hario <hariomarlon83@gmail.com>
2021-11-04 19:56:41 +0800

    Break the loop into separate tests.

--- packages/e2e-tests/specs/admin/events/overview/event-list/search.test.ts
+++ packages/e2e-tests/specs/admin/events/overview/event-list/search.test.ts
@@ -1,38 +1,65 @@
 import { createNewEvent, searchEvent } from '@e2eUtils/admin/events';
 import { Goto } from '@e2eUtils/admin';
 
-beforeAll(async () => {
-	await Goto.eventsListPage();
-});
-
 describe('Search events', () => {
-	it('tests search events', async () => {
-		// Initialize event title to be created for search testing
-		const eventList = ['Test One', 'Test Two', 'Test Three', 'Test Four'];
-		// Convert event titles into uppercase for search testing
-		const searchByUppercase = eventList.map((name) => name.toUpperCase());
-		// Convert event titles into lowercase for search testing
-		const searchByLowerCase = eventList.map((name) => name.toLowerCase());
-		// Cut random index in even title for search testing
-		const searchByPartial = eventList.map((event) => event.slice(0, Math.floor(Math.random() * 6 + 1)));
-		//Generate random string or null value  for search testing
-		const searchByRadmonTextNull = [(Math.random() + 1).toString(36).substring(7), ''];
-		//Accumulate all search cases
-		const searchCases = [eventList, searchByPartial, searchByUppercase, searchByLowerCase, searchByRadmonTextNull];
+	// Initialize event title to be created for search testing
+	const eventList = ['Test One', 'Test Two', 'Test Three', 'Test Four'];
 
+	beforeAll(async () => {
 		// Loop and create event base on the eventList
 		for (const title of eventList) {
 			await createNewEvent({ title });
 		}
 		await Goto.eventsListPage();
-
-		// Loop all search cases
-		for (const [index, value] of searchCases.entries()) {
-			for (const searchText of value) {
-				// handle searching events
-				const serachResult = await searchEvent(searchText, index);
-				expect(serachResult).toBeTruthy();
-			}
-		}
 	});
+
+	const testCases = [
+		// Search by event list titles
+		{
+			desc: 'Search by event titles',
+			data: eventList,
+			expected: true,
+		},
+		// Convert event titles into uppercase for search testing
+		{
+			desc: 'Search by uppercase text',
+			data: eventList.map((name) => name.toUpperCase()),
+			expected: true,
+		},
+		// Convert event titles into lowercase for search testing
+		{
+			desc: 'Search by lowercase text',
+			data: eventList.map((name) => name.toLowerCase()),
+			expected: true,
+		},
+		// Cut random index in even title for search testing
+		{
+			desc: 'Search by partial text',
+			data: eventList.map((event) => event.slice(0, Math.floor(Math.random() * 6 + 1))),
+			expected: true,
+		},
+		// Generate random string for search testing
+		{
+			desc: 'Search by random text',
+			data: [(Math.random() + 1).toString(36).substring(7), 'eweryhfghret', 'asda54h'],
+			expected: false,
+		},
+		// null or empty strings for search testing
+		{
+			desc: 'Search by empty text',
+			data: [null, '', undefined],
+			expected: false,
+		},
+	];
+
+	// // Loop all search cases
+	for (const { desc, data, expected } of testCases) {
+		it(desc, async () => {
+			for (const searchText of data) {
+				// handle searching events
+				const serachResult = await searchEvent(searchText, expected);
+				serachResult ? expect(serachResult).toBeTruthy() : expect(serachResult).toBeFalsy();
+			}
+		});
+	}
 });
