b3789da861d63baa13470053b519b78c7d6bbc00
Marlon Hario <hariomarlon83@gmail.com>
2021-11-05 14:11:14 +0800

    Remove exported searchEvent function.

--- packages/e2e-tests/utils/admin/events/list/index.ts
+++ packages/e2e-tests/utils/admin/events/list/index.ts
@@ -2,4 +2,3 @@ export * from './assertFilteredItems';
 export * from './assertSelectedDefaultOption';
 export * from './createNewEvent';
 export * from './resetFilter';
-export * from './searchEvent';
