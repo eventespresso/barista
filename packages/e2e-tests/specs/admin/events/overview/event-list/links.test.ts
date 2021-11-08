69754ef88cc9141b9b092d3905c3994a6df683df
Marlon Hario <hariomarlon83@gmail.com>
2021-11-09 00:21:24 +0800

    Fix request changes but still need to cleanup code.

--- packages/e2e-tests/specs/admin/events/overview/event-list/links.test.ts
+++ packages/e2e-tests/specs/admin/events/overview/event-list/links.test.ts
@@ -5,7 +5,7 @@ import { ElementHandle } from 'playwright-core';
 
 beforeAll(async () => {
 	// Loop and create event base on the eventList
-	for (const args of eventList) {
+	for (const args of [...eventList, ...eventList]) {
 		await createNewEvent(args);
 	}
 	await Goto.eventsListPage();
@@ -23,51 +23,66 @@ describe('Events overview clickable actions/links', () => {
 	const tableRowsChecker = async () => {
 		const tableRows = await page.$$('table.wp-list-table tbody#the-list tr');
 		const hasNoItems = (await tableRows[0].innerText()).includes('No items found.');
-		// const lolsss = (await tableRows[0].innerText()).includes('Test One');
 
 		const count = hasNoItems ? 0 : tableRows.length;
 
 		return { tableRows, count };
 	};
 
+	const moveToTrash = async () => {
+		await page.selectOption('select#bulk-action-selector-', 'trash_events');
+		await Promise.all([page.waitForNavigation(), page.click('input#doaction')]);
+	};
+
 	it('View all events', async () => {
 		await Promise.all([page.waitForNavigation(), page.click('a:has-text("View All Events")')]);
+		let titleList: string[];
 
 		const { tableRows, count } = await tableRowsChecker();
 
 		if (count) {
 			expect(count).toBeGreaterThanOrEqual(1);
-			// const sadasd = await Promise.all(
-			// 	tableRows.map(async (row) => await row?.$('td.column-name a:has-text("Test One")'))
-			// );
 
-			// const addd = await Promise.all(
-			// 	sadasd.map(async (row) => (await row?.$('td.column-name a.row-title')).innerText())
-			// );
-			// console.log({ addd });
+			const filteredRows = (
+				await Promise.all(
+					tableRows.map(async (row) => {
+						const title = await (await row.$('td.column-name a.row-title')).innerText();
+						return title === 'Test One' ? row : null;
+					})
+				)
+			).filter(Boolean);
 
-			// const daw = await Promise.all(
-			// 	tableRows.filter(async (row) => (await row.innerText()).includes('Test One'))
-			// );
+			const fetchInputValue = await Promise.all(
+				filteredRows.map(
+					async (ro) => await (await ro.$('.check-column input[type="checkbox"]')).getAttribute('value')
+				)
+			);
 
-			// const lagat = await Promise.all(
-			// 	daw.map(async (row) => (await row.$('td.column-name a.row-title')).innerText())
-			// );
-			// console.log({ lagat });
-			// await page.check('input#cb-select-all-1');
-
-			// console.log({ daw });
-			await page.check('input#cb-select-all-1');
-			expect(await page.isChecked('input#cb-select-all-1')).toBeTruthy();
-			await page.selectOption('select#bulk-action-selector-', 'trash_events');
-			await Promise.all([page.waitForNavigation(), page.click('input#doaction')]);
-
-			const { tableRows: rowsChecker, count: countCkecker } = await tableRowsChecker();
-
-			if (countCkecker) {
-				const titleList = await rowTitleList(rowsChecker);
-				expect(titleList).toBe(0);
+			for (const iterator of fetchInputValue) {
+				await page.check(`.check-column input[value="${iterator}"]`);
 			}
+
+			await moveToTrash();
+			const { tableRows: removeIndividual } = await tableRowsChecker();
+
+			// const dawwwww = await Promise.all(
+			// 	rowsChecker.map(async (ro) => await (await ro.$('td.column-name a.row-title')).innerText())
+			// );
+			titleList = await rowTitleList(removeIndividual);
+			expect(titleList).not.toContain('Test One');
+			await page.check('input#cb-select-all-1');
+			await moveToTrash();
+
+			// expect(titleList).toBe(0);
+
+			const { tableRows: removeAll, count: countCkecker } = await tableRowsChecker();
+			// titleList = await rowTitleList(removeAll);
+			// console.log({ countCkecker });
+			// if (!countCkecker) {
+			// 	const titleList = await rowTitleList(removeAll);
+			expect(countCkecker).toBe(0);
+			// 	console.log({ titleList });
+			// }
 		} else {
 			expect(count).toBe(0);
 		}
