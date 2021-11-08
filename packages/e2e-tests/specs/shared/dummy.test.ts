dbfa0a387a9c5c88248edd5c6e1aa23d30c06066
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-11-08 22:51:47 +0530

    Restore dummy test

--- packages/e2e-tests/specs/shared/dummy.test.ts
+++ packages/e2e-tests/specs/shared/dummy.test.ts
@@ -1,21 +1,8 @@
-// import { createNewEvent } from '@e2eUtils/admin/events';
-import { Goto, EventListSurfer } from '@e2eUtils/admin';
-
-const eventListSurfer = new EventListSurfer();
+import { createNewEvent } from '@e2eUtils/admin/events';
 
 describe('DummyTest', () => {
 	it('Does nothing more than creating an event!', async () => {
-		// await createNewEvent({ title: 'Dummy Event', description: 'some desc' });
-
-		await Goto.eventsListPage();
-
-		const href = await eventListSurfer.getViewLinkByText('Trash');
-
-		await eventListSurfer.selectBulkAction({ label: 'Move to Trash' });
-		await eventListSurfer.selectAll();
-		await eventListSurfer.applyBulkAction();
-
-		console.log({ href });
+		await createNewEvent({ title: 'Dummy Event', description: 'some desc' });
 
 		expect(true).toBe(true);
 	});
