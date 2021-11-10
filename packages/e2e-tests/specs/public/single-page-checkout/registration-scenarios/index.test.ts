1018d716ffe38d9ab8ea98d21f210ab956afa3f2
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-11-10 16:41:43 +0530

    Remove title assertion and improve content assertion

--- packages/e2e-tests/specs/public/single-page-checkout/registration-scenarios/index.test.ts
+++ packages/e2e-tests/specs/public/single-page-checkout/registration-scenarios/index.test.ts
@@ -36,10 +36,8 @@ describe(namespace, () => {
 			},
 		});
 
-		const title = await page.$eval('h1.entry-title', (el) => el.textContent);
-		expect(title).toContain('Thank You');
-
 		const content = await page.$eval('.entry-content', (el) => el.textContent);
-		expect(content).toContain('Congratulations');
+		expect(content).toContain('Your registration has been successfully processed.');
+		expect(content).toContain('View Full Order Confirmation Receipt');
 	});
 });
