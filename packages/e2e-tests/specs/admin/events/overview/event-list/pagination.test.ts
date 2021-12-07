dc74d0772918606a7861291e2e061cafb0bb0ea3
Marlon Hario <hariomarlon83@gmail.com>
2021-12-08 00:25:57 +0800

    Finalize code for pagination.

--- packages/e2e-tests/specs/admin/events/overview/event-list/pagination.test.ts
+++ packages/e2e-tests/specs/admin/events/overview/event-list/pagination.test.ts
@@ -1,4 +1,4 @@
-import { createNewEvent } from '@e2eUtils/admin/events';
+import { createMultipleEvents } from '@e2eUtils/admin/events';
 import { EventsListSurfer, Goto } from '@e2eUtils/admin';
 import { eventData } from '../../../../shared/data';
 
@@ -10,77 +10,65 @@ beforeAll(async () => {
 	await eventsListSurfer.deleteAllEventsByLink('View All Events');
 	// delete permanently all events at trash link
 	await eventsListSurfer.deleteAllPermanentlyFromTrash();
-
-	// generate lots of events for pagination
-	// let events = Object.values(eventData);
-	// events = [...events, ...events, ...events];
-	// for (const event of events) {
-	// 	await createNewEvent(event);
-	// }
 });
 
 describe('Test overview pagination', () => {
-	// let events = Object.values(eventData);
-	// events = [...events, ...events, ...events];
+	let events = Object.values(eventData);
+	events = [...events, ...events, ...events];
 	let countEventsBeforeAdding: number;
 	let countAfterAddedEvents: number;
+	let totalPages: number;
+	let defaultPerPage: number;
 
 	it('Count how many events from "Vew all events"', async () => {
+		// count the initial events in view all events, it suppose to start with zero
 		countEventsBeforeAdding = await eventsListSurfer.getViewCount('View All Events');
+		// assert starting count of events
 		expect(countEventsBeforeAdding).toBe(0);
 	});
 
 	it('Add lots of events for pagination', async () => {
 		// generate lots of events for pagination
-		let events = Object.values(eventData);
-		events = [...events, ...events, ...events];
-		console.log({ events: events.length });
-
+		await Promise.all([page.waitForNavigation(), page.click('#add-new-event')]);
 		for (const event of events) {
-			await createNewEvent(event);
+			// create new event
+			await createMultipleEvents(event);
 		}
+		await Goto.eventsListPage();
+		// view and count events at view all events
 		countAfterAddedEvents = await eventsListSurfer.goToViewAndCount('View All Events');
-		const countAddedEvent = countAfterAddedEvents - countEventsBeforeAdding;
-		expect(countAfterAddedEvents).toBe(countAddedEvent + countEventsBeforeAdding);
-		// expect(0).toBe(0);
+		// assert inserted events
+		expect(countAfterAddedEvents).toBe(events.length);
 	});
 
 	it('Get total pages', async () => {
-		// countAllEventsList = await eventsListSurfer.goToViewAndCount('View All Events');
-		const defaultPerPage = await eventsListSurfer.getDefaultPerPage();
-		const totalPages = await eventsListSurfer.getTotalPagePagination();
-		console.log({ daw: Math.ceil(countAfterAddedEvents / defaultPerPage) });
+		// get the default per page
+		defaultPerPage = await eventsListSurfer.getDefaultPerPage();
+		// get total page for pagination
+		totalPages = await eventsListSurfer.getTotalPagePagination();
+		// assert total page count
+		expect(totalPages).toBe(Math.ceil(countAfterAddedEvents / defaultPerPage));
+	});
 
-		// 	// // count all events from view all events
-		// 	// const countAllEventsList = await eventsListSurfer.goToViewAndCount('View All Events');
-		// 	// // click screen option to get the pagination set
-		// 	// await page.click('#show-settings-link');
-		// 	// // get the default per page
-		// 	// const paginationEvents = await (await page.$('#espresso_events_default_per_page')).getAttribute('value');
-		// 	// // get how many set of page in pagination
-		// 	// const totalPages = await (await page.$('span.total-pages')).innerText();
-		// 	// // get the remainder countAllEventsList modulus to default per page
-		// 	// const getPageRemainder = countAllEventsList % Number(paginationEvents);
-		// 	// // this will get the number of event in the last pagiantion (this will use for assertion)
-		// 	// const countLastPageRows = 0;
-
-		// 	// // loop the pagination per page
-		// 	// for (let pages = 1; pages < Number(totalPages); pages++) {
-		// 	// 	// first get the link for next page in pagination
-		// 	// 	const href = await (await page.$('.pagination-links a.next-page')).getAttribute('href');
-		// 	// 	// go to next page link
-		// 	// 	await Promise.all([page.waitForLoadState(), page.goto(href)]);
-		// 	// 	// count event every paginate page
-		// 	// 	const countRowsPerPage = await eventsListSurfer.getItemCount();
-		// 	// 	// get the remainder per page by countRowsPerPage modulus to default per page
-		// 	// 	countLastPageRows = countRowsPerPage % Number(paginationEvents);
-		// 	// 	// assert event count every page pagination
-		// 	// 	expect(countRowsPerPage).toBeGreaterThanOrEqual(countLastPageRows);
-		// 	// }
-
-		// 	// // assert the getPageRemainder and countLastPageRows this will always be equal
-		// 	// expect(getPageRemainder).toBe(countLastPageRows);
-
-		expect(0).toBe(0);
+	it('Test pagination', async () => {
+		// loop the pagination per page
+		for (let pages = 1; pages < totalPages; pages++) {
+			// first get the link for next page in pagination
+			const href = await (await page.$('.pagination-links a.next-page')).getAttribute('href');
+			// go to next page link
+			await Promise.all([page.waitForLoadState(), page.goto(href)]);
+			// count event every paginate page
+			const countRowsPerPage = await eventsListSurfer.getItemCount();
+			// get the remainder per page by countRowsPerPage modulus to default per page
+			const countLastPageRows = countRowsPerPage % defaultPerPage;
+			// assert event count every page pagination
+			if (totalPages - 1 === pages) {
+				// assert last page
+				expect(countLastPageRows).toBe(countLastPageRows);
+			} else {
+				// assert page count
+				expect(countRowsPerPage).toBe(defaultPerPage);
+			}
+		}
 	});
 });
