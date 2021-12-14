76e19e40a218a5ebc032a9b895a6dcce9494c090
Brent Christensen <brent@eventespresso.com>
2021-12-14 13:23:49 -0800

    get venue thumbnail and pass to entity card

--- domains/core/admin/eventEditor/src/ui/datetimes/datesList/cardView/DateCard.tsx
+++ domains/core/admin/eventEditor/src/ui/datetimes/datesList/cardView/DateCard.tsx
@@ -1,7 +1,10 @@
+import { useMemo } from 'react';
+
 import { EntityActionsMenuLayout } from '@eventespresso/ui-components';
 import { datetimeStatusBgColorClassName } from '@eventespresso/helpers';
 import { EntityCard } from '@eventespresso/ui-components';
-import { useDatetimeItem } from '@eventespresso/edtr-services';
+import { useDatetimeItem, useVenues } from '@eventespresso/edtr-services';
+import { findEntityByGuid } from '@eventespresso/predicates';
 
 import DateActionsMenu from '../actionsMenu/DateActionsMenu';
 import DateCardSidebar from './DateCardSidebar';
@@ -11,9 +14,15 @@ import type { DateItemProps } from '../types';
 const DateCard: React.FC<DateItemProps> = ({ id }) => {
 	const date = useDatetimeItem({ id });
 	const bgClassName = datetimeStatusBgColorClassName(date);
+	const selectedVenueId = date?.venue;
+
+	const venues = useVenues();
+	const selectedVenue = useMemo(() => findEntityByGuid(venues)(selectedVenueId), [selectedVenueId, venues]);
+	const thumbnail = selectedVenue?.thumbnail;
 
 	return date ? (
 		<EntityCard
+			bg={thumbnail}
 			actionsMenu={<DateActionsMenu entity={date} layout={EntityActionsMenuLayout.Vertical} />}
 			details={<Details entity={date} />}
 			entity={date}
