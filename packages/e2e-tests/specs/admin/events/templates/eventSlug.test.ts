f0bade37e642d3d356dd40555f2afcccd362e2b6
Marlon Hario <hariomarlon83@gmail.com>
2022-01-10 21:53:03 +0800

    Add comment.

---
+++ packages/e2e-tests/specs/admin/events/templates/eventSlug.test.ts
@@ -0,0 +1,45 @@
+import { saveVideo, PageVideoCapture } from 'playwright-video';
+import { Goto, TemplatesManager } from '@e2eUtils/admin';
+
+const templatesManager = new TemplatesManager();
+
+const namespace = 'templates-event-listing-url';
+let capture: PageVideoCapture;
+
+beforeAll(async () => {
+	capture = await saveVideo(page, `artifacts/${namespace}.mp4`);
+	await Goto.eventsListPage();
+	await templatesManager.gotoTemplates();
+});
+
+afterAll(async () => {
+	await capture?.stop();
+});
+
+describe('Event listings URL test', () => {
+	let getBaseUrl: string;
+	let getEventSlug: string;
+
+	it('Get the base url', async () => {
+		// Get base URL at templates tab event listing pages
+		getBaseUrl = await templatesManager.getBaseUrl();
+		// assert base url if it is not empty
+		expect(getBaseUrl).toBeTruthy();
+	});
+
+	it('Get the url slug', async () => {
+		// Get event slug at templates tab event listing pages
+		getEventSlug = await templatesManager.getEventSlug();
+		// assert event slug if it is not empty
+		expect(getEventSlug).toBeTruthy();
+	});
+
+	it('Get event listing url', async () => {
+		// Get event listing URL at templates tab event listing pages
+		const getEventListingUrl = await templatesManager.getEventListingUrl();
+		// compose url and compare to event listing url
+		const toCompareUrl = `${getBaseUrl.trim()}${getEventSlug && getEventSlug.trim()}/`;
+		// assert compose url if equal to event listing url
+		expect(getEventListingUrl).toBe(toCompareUrl);
+	});
+});
