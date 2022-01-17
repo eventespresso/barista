d3c953e7c995c912efa7c92405a33aeffa074ce7
Marlon Hario <hariomarlon83@gmail.com>
2022-01-17 22:55:33 +0800

    Refactor code and remove code not part of the test.

--- packages/e2e-tests/specs/admin/events/templates/archives/displayTicketSelector.test.ts
+++ packages/e2e-tests/specs/admin/events/templates/archives/displayTicketSelector.test.ts
@@ -1,9 +1,8 @@
 import { saveVideo, PageVideoCapture } from 'playwright-video';
-import { TemplatesManager, VenuesManager } from '@e2eUtils/admin';
-import { eventData, eventVenueData } from '../../../../shared/data';
+import { TemplatesManager, Goto, createNewEvent } from '@e2eUtils/admin';
+import { eventData } from '../../../../shared/data';
 
 const templatesManager = new TemplatesManager();
-const venuesManager = new VenuesManager();
 
 const namespace = 'templates-archives-display-ticket-selector';
 let capture: PageVideoCapture;
@@ -17,47 +16,54 @@ afterAll(async () => {
 });
 
 describe('Display ticket selector - archives test', () => {
-	it('Create new venue', async () => {
-		// this function is to delete all venues first then create one and return the before and after count venue
-		const { countAfterCreate, countBeforeCreate, addedVenue } = await venuesManager.processToCreateNewVenue(
-			eventVenueData
-		);
-		// assert added venue
-		expect(countAfterCreate).toBe(countBeforeCreate + addedVenue);
-		expect(addedVenue).not.toBe(0);
-	});
-
-	it('Create new event and set created venue', async () => {
-		// this function is to create new event first then assign the venue that already created then return before and after count event
-		const { countAfterCreate, countBeforeCreate, addedEvent, getVenueTitle } =
-			await venuesManager.processToAssignVenueAtEvent({
-				...eventData.upcoming,
-				shouldPublish: false,
-				venueTitle: eventVenueData.title,
-			});
-		// assert added event
-		expect(countAfterCreate).toBe(countBeforeCreate + addedEvent);
-		// assert venue title at first event
-		expect(eventVenueData.title).toBe(getVenueTitle);
-		expect(countAfterCreate).not.toBe(0);
-	});
-
-	it('Set display ticket selector to "Yes"', async () => {
+	// set ticket selector and get ticket container at to event listing url
+	const processToSetTicketSelector = async ({ value, textValue }: { value: string; textValue: string }) => {
 		//  go to templates tab
 		await templatesManager.gotoTemplates();
 		// gett selected display ticket selector at templates archive section
 		const getSelectedDisplayDescription = await templatesManager.getSelectedDisplayTicketSelector();
 		// check if selected ticket selector is equal to "No" if true se to "Yes" else do nothing
-		if (getSelectedDisplayDescription !== 'Yes') {
+		if (getSelectedDisplayDescription !== textValue) {
 			// set display ticket selector to "Yes"
-			await templatesManager.setAndSaveDisplayTicketSelector({ value: '1' });
+			await templatesManager.setAndSaveDisplayTicketSelector({ value });
 		}
 		// go to event listing url
 		const getEventListingUrl = await templatesManager.getEventListingUrl();
 		await page.goto(getEventListingUrl);
+
 		// get ticket container at to event listing url
-		const getTicketContainer = await page.$('.tkt-slctr-tbl-wrap-dv');
-		// assert if ticket container is exist
+		return await page?.$('.tkt-slctr-tbl-wrap-dv');
+	};
+
+	it('Create new sample event', async () => {
+		await Goto.eventsListPage();
+		// count event from view all event link action
+		const countEvent = await templatesManager.getViewCount('View All Events');
+		if (!countEvent) {
+			await createNewEvent(eventData.upcoming);
+			await Goto.eventsListPage();
+		}
+		// count event from view all event link action after created one
+		const countEventAfter = await templatesManager.getViewCount('View All Events');
+
+		// assert count from view all event link
+		expect(countEventAfter).toBeTruthy();
+	});
+
+	it('Set display ticket selector to "Yes"', async () => {
+		// set ticket selector and get ticket container at to event listing url
+		const getTicketContainer = await processToSetTicketSelector({ value: '1', textValue: 'Yes' });
+
+		// assert if ticket container is exist after selecting display to "Yes"
 		expect(getTicketContainer).toBeTruthy();
+	});
+
+	it('Set display ticket selector to "No"', async () => {
+		await Goto.eventsListPage();
+		// set ticket selector and get ticket container at to event listing url
+		const getTicketContainer = await processToSetTicketSelector({ value: '0', textValue: 'No' });
+
+		// assert if ticket container still exist, suppose to be null after selected "No"
+		expect(getTicketContainer).toBe(null);
 	});
 });
