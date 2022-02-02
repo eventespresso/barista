b6ffac75b9a0c513401bef6d9d828fe5eded35ee
Marlon Hario <hariomarlon83@gmail.com>
2022-02-03 01:47:29 +0800

    Finalize code.

--- packages/e2e-tests/specs/admin/events/templates/ticket-selector/showDateAndTimeFilter.test.ts
+++ packages/e2e-tests/specs/admin/events/templates/ticket-selector/showDateAndTimeFilter.test.ts
@@ -1,20 +1,9 @@
 import { saveVideo, PageVideoCapture } from 'playwright-video';
-import { addDays, addHours, subDays, subHours } from 'date-fns';
+import { addDays } from 'date-fns';
 import { NOW } from '@eventespresso/constants';
-import {
-	Goto,
-	TemplatesManager,
-	EventsListSurfer,
-	createNewEvent,
-	createMultipleEvents,
-	fillEventFields,
-	EDTRGlider,
-	DateFormatter,
-} from '@e2eUtils/admin';
+import { Goto, TemplatesManager, EventsListSurfer, createNewEvent, DateFormatter } from '@e2eUtils/admin';
 import { eventData } from '../../../../shared/data';
-// import { EDTRGlider } from '../editor';
 
-const edtrGlider = new EDTRGlider();
 const templatesManager = new TemplatesManager();
 const eventsListSurfer = new EventsListSurfer();
 
@@ -22,21 +11,19 @@ const namespace = 'templates-ticket-selector-show-date-and-time-filter';
 let capture: PageVideoCapture;
 
 beforeAll(async () => {
-	// capture = await saveVideo(page, `artifacts/${namespace}.mp4`);
-	// await eventsListSurfer.cleanUpEvents();
+	capture = await saveVideo(page, `artifacts/${namespace}.mp4`);
 	await eventsListSurfer.deleteAllEventsByLink('View All Events');
 	await Goto.eventsListPage();
 });
 
 afterAll(async () => {
-	// await capture?.stop();
+	await capture?.stop();
 });
 
 describe('Show date and time filter - ticket selector test', () => {
 	it('Create new sample events for date filter', async () => {
 		const events = Object.values(eventData);
 		const slicedEvents: any = events.slice(0, 2);
-		// console.log({ slicedEvents });
 
 		// count event first before creating one
 		const countBeforeChecking = await templatesManager.goToViewAndCount('View All Events');
@@ -54,91 +41,119 @@ describe('Show date and time filter - ticket selector test', () => {
 			const restoreLink = await templatesManager.getItemActionLinkByText(firstItem, 'Edit');
 			await page.goto(restoreLink);
 		}
-		// await eventsListSurfer.removeEventDatesFilter();
-		await eventsListSurfer.setAndSaveEventDates({ ...eventData.upcoming });
+		// count event date before creating new 2 event dates
+		const countEventDateBeforeCreate = await eventsListSurfer.countEventDates();
+		// assert event dates before creating new, suppose to be equal to one for default existing one event date
+		expect(countEventDateBeforeCreate).toBe(1);
 
+		// create 2 sample event dates for date filter
 		for (const iterator of slicedEvents) {
 			await eventsListSurfer.newEventDates(iterator);
 		}
-		// await eventsListSurfer.removeEventTicketDatesFilter();
-		console.log({ slicedEvents: slicedEvents.length });
 
+		// count event date after creating new 2 event dates
+		const countEventDateAfterCreate = await eventsListSurfer.countEventDates();
+		// assert count after create, suppose to be count is equal to 3
+		expect(countEventDateAfterCreate).toBe(slicedEvents.length + countEventDateBeforeCreate);
+
+		// count tickets before creating new 3 tickets
+		const countTicketBeforeCreate = await eventsListSurfer.countTickets();
+		// assert count ticket before create new, suppose to be count ticket is equal to one by default
+		expect(countTicketBeforeCreate).toBe(1);
+
+		// create 3 sample tickets for date filter
 		for (const iterator of [1, 2, 3]) {
-			// startDate: DateFormatter.eventDateFormat(subDays(NOW, 1)),
-			// endDate: DateFormatter.eventDateFormat(addDays(NOW, 1)),
-
-			// await eventsListSurfer.setAndSaveEventTicketDates({
-			// 	...eventData.upcoming,
-			// 	startDate: DateFormatter.eventDateFormat(addDays(NOW, iterator * 30)),
-			// 	shouldPublish: false,
-			// });
-			console.log({ iterator: iterator });
-
-			// await eventsListSurfer.clickAddNewTicket();
-			await page.click('text=Add New Ticket');
-			// const daw = DateFormatter.eventDateFormat(addDays(NOW, iterator * 30));
-			// await page.waitForSelector('.sections-wrapper');
-
-			// await page.focus('.sections-wrapper #endDate');
-			// await page.fill(
-			// 	'.sections-wrapper #endDate',
-			// 	DateFormatter.eventDateFormat(addDays(NOW, iterator * 30 + 1))
-			// );
-			// await page.focus('.sections-wrapper .section-body #name');
-			await page.click('#startDate');
-			await page.fill('#startDate', DateFormatter.eventDateFormat(addDays(NOW, iterator * 30)));
-			await page.click('#endDate');
-			await page.fill('#endDate', DateFormatter.eventDateFormat(addDays(NOW, iterator * 30 + 1)));
-			await page.fill('.sections-wrapper .section-body #name', 'Ticket title');
-			// await page.focus('.sections-wrapper .public-DraftEditor-content');
-			await page.fill('.sections-wrapper .public-DraftEditor-content', 'Ticket description');
-			// await page.focus('.sections-wrapper #startDate');
-
-			await page.click('text=Skip prices - assign dates');
-			await page.click('#ee-ticket-assignments-manager-table-data-cell-row-0-col-1 button');
-			await page.click('.ee-modal__footer button[type="submit"]');
-			await page.waitForSelector('#ee-event-editor');
-			// await Promise.all([page.waitForNavigation(), page.click('.ee-modal__footer button[type="submit"]')]);
-
-			console.log({ iterator: DateFormatter.eventDateFormat(addDays(NOW, iterator * 30 + iterator)) });
-			// console.log({ iterator: DateFormatter.eventDateFormat(addDays(NOW, iterator * 30)) });
-			// await eventsListSurfer.newEventDates(iterator);
+			// This function is to trigger add new ticket then save after fill in all required fields
+			await eventsListSurfer.newTicket({
+				startDate: DateFormatter.eventDateFormat(addDays(NOW, iterator * 30)),
+				endDate: DateFormatter.eventDateFormat(addDays(NOW, iterator * 30 + 1)),
+				ticketTitle: 'Ticket test',
+				ticketDescription: 'Ticket test description',
+			});
 		}
 
+		// publish after creating event dates and tickets
 		await eventsListSurfer.publishEventChanges(true);
+		// wait edtr container to load
+		await page.waitForSelector('#ee-event-editor');
 
-		expect(1).toBeTruthy();
+		// count tickets after creating new 3 tickets
+		const countTicketAfterCreate = await eventsListSurfer.countTickets();
+		// assert count ticket after create new 3 ticket, suppose to be equal to 4 because there is already existing 1 by default
+		expect(countTicketAfterCreate).toBe(countTicketBeforeCreate + 3);
+
+		// go back to event list main page
+		await Goto.eventsListPage();
+
+		// count event first before creating one
+		const countAfterCreateEvent = await templatesManager.goToViewAndCount('View All Events');
+		//assert count event
+		expect(countAfterCreateEvent).toBe(1);
 	});
 
 	it('Set date & time filter threshold to "2"', async () => {
-		await Goto.eventsListPage();
 		await templatesManager.gotoTemplates();
+		// set and save for date & time filter threshold at ticket selector template settings
 		await templatesManager.setAndSaveDateAndTimeFilterThreshold({ value: '2' });
+		// get selected value for date & time filter threshold at ticket selector template settings
 		const getSelectedValue = await templatesManager.getSelectedDateAndTimeFilterThreshold();
-		console.log({ getSelectedValue });
-
+		// assert selected threshold
 		expect(getSelectedValue).toBe('2');
 	});
 
 	it('Set show date & time filter to "Maybe show date & time filter"', async () => {
+		// set and save for show date & time filter at ticket selector template settings
 		await templatesManager.setAndSaveShowDateAndTimeFilter({ value: 'maybe_datetime_selector' });
+		// get selected value for show date & time filter  at ticket selector template settings
 		const getSelectedValue = await templatesManager.getSelectedShowDateAndTimeFilter();
-		console.log({ getSelectedValue });
+		// assert selected filter
+		expect(getSelectedValue).toBe('Maybe show date & time filter');
+
 		// go to event listing page
 		await templatesManager.gotoEventListing();
+		// get filter text
 		const getTextLabel = await (await page.$('text= Filter by Date')).innerText();
-		console.log({ getTextLabel });
-		const getFilterList = await page.$$('.checkbox-dropdown-selector ul li');
-		console.log({ getFilterList: getFilterList.length });
-		for (const filter of getFilterList) {
-			// await filter.uncheck('input.datetime-selector-option');
-			const getFilterLabel = await (await filter.$('.datetime-selector-option-text-spn')).innerText();
-			console.log(getFilterLabel);
-		}
-		const countSelectorShow = await page.$$('#tkt-slctr-tbl-3590.tkt-slctr-tbl > tbody > tr');
-		console.log({ countSelectorShow: countSelectorShow.length });
+		// assert innert text for filter button
+		expect(getTextLabel.trim()).toBe('Filter by Date');
 
-		await page.click('text= Filter by Date');
-		expect(getSelectedValue).toBe('Maybe show date & time filter');
+		// trigger filter dropdown
+		await page.click('.datetime_selector-dv button.checkbox-dropdown-selector-btn');
+		// get all filter dropdown list
+		const getFilterList = await page?.$$('.checkbox-dropdown-selector ul li');
+		// assert dropdown list item
+		expect(getFilterList.length).toBeTruthy();
+
+		// count ticket list before uncheck all at event listing page
+		const countSelectorBeforeHidden = await page?.$$('.tkt-slctr-tbl > tbody > tr.ee-hidden-ticket-tr');
+		// assert dropdown before uncheck list item, suppose to be zero because theres no hidden ticket yet
+		expect(countSelectorBeforeHidden.length).toBe(0);
+
+		// uncheck all ticket filter
+		for (const filter of getFilterList) {
+			const checkbox = await filter.$('input.datetime-selector-option');
+			await checkbox.uncheck();
+		}
+		// count ticket list after uncheck all at event listing page
+		const countSelectorAfterHidden = await page?.$$('.tkt-slctr-tbl > tbody > tr.ee-hidden-ticket-tr');
+		// assert dropdown after uncheck list item
+		expect(countSelectorAfterHidden.length).toBeTruthy();
+	});
+
+	it('Set show date & time filter to "Do not show date & time filter"', async () => {
+		await Goto.eventsListPage();
+		await templatesManager.gotoTemplates();
+		// selecte display ticket filter to not display
+		await templatesManager.setAndSaveShowDateAndTimeFilter({ value: 'no_datetime_selector' });
+		// get selected show date & time filter
+		const getSelectedValue = await templatesManager.getSelectedShowDateAndTimeFilter();
+		// assert selected value
+		expect(getSelectedValue).toBe('Do not show date & time filter');
+
+		// go to event listing page
+		await templatesManager.gotoEventListing();
+		// check if filter still exist suppose to be null
+		const getTextLabel = await page?.$('text= Filter by Date');
+		// assert checking filter
+		expect(getTextLabel).toBeNull();
 	});
 });
