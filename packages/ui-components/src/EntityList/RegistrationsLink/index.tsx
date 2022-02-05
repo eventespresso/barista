40f5a5f40fb72cb76d91d03bf680911a49b4ca7a
Brent Christensen <brent@eventespresso.com>
2022-02-04 18:17:20 -0800

    add tooltip and fix styles

--- packages/ui-components/src/EntityList/RegistrationsLink/index.tsx
+++ packages/ui-components/src/EntityList/RegistrationsLink/index.tsx
@@ -1,18 +1,19 @@
 import classnames from 'classnames';
 
+import { __ } from '@eventespresso/i18n';
 import { Groups } from '@eventespresso/icons';
 
-import { Link } from '../../..';
+import { TextLink } from '../../..';
 
 import './style.scss';
 
-const RegistrationsLink: React.FC<React.ComponentProps<typeof Link>> = ({ href, children, ...props }) => {
-	const className = classnames('ee-editor-details-reg-url-link', props.className);
-
+const RegistrationsLink: React.FC<React.ComponentProps<typeof TextLink>> = ({ href, children, ...props }) => {
+	const className = classnames('ee-editor-details-reg-url-link', !children && 'ee-icon-button', props.className);
+	const tooltip = __('click to open the registrations admin page in a new tab or window');
 	return (
-		<Link {...props} className={className} href={href}>
+		<TextLink {...props} className={className} href={href} tooltip={tooltip}>
 			{children || <Groups />}
-		</Link>
+		</TextLink>
 	);
 };
 
