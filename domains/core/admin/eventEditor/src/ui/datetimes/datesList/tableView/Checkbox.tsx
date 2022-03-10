e6a3449cf7b7b74e8587a56100da77b06c326d08
Brent Christensen <brent@eventespresso.com>
2022-03-10 09:10:47 -0800

    Add withPermission HOC and Fix New EDTR Caps (#1139)

--- domains/core/admin/eventEditor/src/ui/datetimes/datesList/tableView/Checkbox.tsx
+++ domains/core/admin/eventEditor/src/ui/datetimes/datesList/tableView/Checkbox.tsx
@@ -1,6 +1,6 @@
 import { ActionCheckbox } from '@eventespresso/ee-components';
 import { useVisibleDatetimeIds } from '@eventespresso/edtr-services';
-import { withFeature } from '@eventespresso/services';
+import { withPermission } from '@eventespresso/services';
 import type { ActionCheckboxProps } from '@eventespresso/ee-components';
 
 const Checkbox: React.FC<ActionCheckboxProps> = (props) => {
@@ -9,4 +9,4 @@ const Checkbox: React.FC<ActionCheckboxProps> = (props) => {
 	return <ActionCheckbox {...props} visibleEntityIds={visibleDatetimeIds} />;
 };
 
-export default withFeature('ee_event_editor_bulk_edit')(Checkbox);
+export default withPermission('ee_event_editor_bulk_edit')(Checkbox);
