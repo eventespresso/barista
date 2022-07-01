e1380dd0186c8b8a446b1222e931b07667c43058
majidabbasi96 <majid.abbasi.56@gmail.com>
2022-07-02 00:45:43 +0400

    Added reset settings for passing this test

--- packages/e2e-tests/specs/public/single-page-checkout/registration-scenarios/sold-out.test.ts
+++ packages/e2e-tests/specs/public/single-page-checkout/registration-scenarios/sold-out.test.ts
@@ -1,15 +1,28 @@
 import { saveVideo } from 'playwright-video';
 
-import { createNewEvent, DateEditor, EDTRGlider } from '@e2eUtils/admin/events';
+import { EventsListSurfer, createNewEvent, DateEditor, EDTRGlider } from '@e2eUtils/admin/events';
 import { EventRegistrar, RegisterOptions } from '@e2eUtils/public/reg-checkout';
 
 const namespace = 'event.free.event.registration.sold.out';
 
+const eventsListSurfer = new EventsListSurfer();
 const registrar = new EventRegistrar();
 const edtrGlider = new EDTRGlider();
 
+import { Goto, DefaultSettingsManager } from '@e2eUtils/admin';
+
+const defaultSettingsManager = new DefaultSettingsManager();
+
 beforeAll(async () => {
 	await saveVideo(page, `artifacts/${namespace}.mp4`);
+	
+	await Goto.eventsListPage();
+
+	await defaultSettingsManager.gotoDefaultSettings();
+	await defaultSettingsManager.selectDefaultRegStatus('RAP');
+
+	await eventsListSurfer.deleteAllEventsByLink('View All Events');
+
 	await createNewEvent({ title: 'Free event' });
 });
 
