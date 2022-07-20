c918d9548bd67a9e095447b3e1baf798e47b19e8
majidabbasi96 <majid.abbasi.56@gmail.com>
2022-07-20 21:23:41 +0400

    moved the Barista activation and Default settings Logic to playwright general

--- packages/e2e-tests/specs/public/single-page-checkout/registration-scenarios/sold-out.test.ts
+++ packages/e2e-tests/specs/public/single-page-checkout/registration-scenarios/sold-out.test.ts
@@ -1,36 +1,26 @@
 import { saveVideo, PageVideoCapture } from 'playwright-video';
-import { Goto, DefaultSettingsManager } from '@e2eUtils/admin';
 import { EventsListSurfer, createNewEvent, DateEditor, EDTRGlider } from '@e2eUtils/admin/events';
 import { EventRegistrar, RegisterOptions } from '@e2eUtils/public/reg-checkout';
-import { activatePlugin, deactivatePlugin } from '@e2eUtils/admin/wp-plugins-page';
-
-const baristaPlugin = 'barista/ee-barista.php';
 
 const eventsListSurfer = new EventsListSurfer();
 const registrar = new EventRegistrar();
 const edtrGlider = new EDTRGlider();
-const defaultSettingsManager = new DefaultSettingsManager();
+const dateEditor = new DateEditor();
 
 const namespace = 'event.free.event.registration.sold.out';
 let capture: PageVideoCapture;
 
 beforeAll(async () => {
 	capture = await saveVideo(page, `artifacts/${namespace}.mp4`);
-	await activatePlugin(baristaPlugin);
 	
-	await Goto.eventsListPage();
-	//go to default settings tab
-	await defaultSettingsManager.gotoDefaultSettings();
-	await defaultSettingsManager.selectDefaultEditor('1');
-	await defaultSettingsManager.selectDefaultRegStatus('RAP');
-	
-
 	await eventsListSurfer.deleteAllEventsByLink('View All Events');
 
 	await createNewEvent({ title: 'Free event' });
 });
 
-const dateEditor = new DateEditor();
+afterAll(async () => {
+	await capture?.stop();
+});
 
 describe(namespace, () => {
 	it('should show show sold out label on date when the number of registration is the same as capacity', async () => {
