c7e13f1e5a7c773e6301564f93f52123463f2986
Brent Christensen <brent@eventespresso.com>
2022-08-12 19:00:25 -0700

    improve alert dialog

--- packages/adapters/src/AlertDialog/types.ts
+++ packages/adapters/src/AlertDialog/types.ts
@@ -1,10 +1,22 @@
 import type { AlertDialogProps as AlertDialogAdapterProps, AlertIconProps } from '@chakra-ui/react';
 
+export enum AlertType {
+	ACCENT = 'accent',
+	DEFAULT = 'default',
+	MINIMAL = 'minimal',
+	PRIMARY = 'primary',
+	SECONDARY = 'secondary',
+}
+
 export interface AlertDialogProps extends Omit<AlertDialogAdapterProps, 'children'> {
+	addIconBG?: boolean;
+	alertType?: AlertType;
 	body?: React.ReactNode;
 	cancelButton: React.ReactNode;
-	contentClassName?: string;
+	className?: string;
+	dialogClassName?: string;
 	header: React.ReactNode;
+	icon?: React.ComponentType<any>;
 	okButton: React.ReactNode;
 }
 
