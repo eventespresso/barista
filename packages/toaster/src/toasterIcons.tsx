46653e7e66d7cf12ae89dcc6695d8ae5b788c70b
Brent Christensen <brent@eventespresso.com>
2022-08-16 10:12:28 -0700

    don't hardcode styles like that

--- packages/toaster/src/toasterIcons.tsx
+++ packages/toaster/src/toasterIcons.tsx
@@ -1,13 +1,11 @@
-import { Check, CloseCircleOutlined, InfoCircleOutlined, Spinner } from '@eventespresso/icons';
-
-const fontSize = '1.2rem';
+import { Check, CloseCircleOutlined, InfoCircleOutlined, Spinner, Warning } from '@eventespresso/icons';
 
 const toasterIcons = {
-	error: <CloseCircleOutlined color='rgb(255, 77, 79)' fontSize={fontSize} />,
-	info: <InfoCircleOutlined color='var(--ee-color-primary)' fontSize={fontSize} />,
-	loading: <Spinner className='ee-loading-spinner' color='var(--ee-color-primary)' fontSize={fontSize} />,
-	success: <Check color='var(--ee-color-primary)' fontSize={fontSize} />,
-	warning: <InfoCircleOutlined color='var(--ee-color-accent)' fontSize={fontSize} />,
+	error: <CloseCircleOutlined />,
+	info: <InfoCircleOutlined />,
+	loading: <Spinner />,
+	success: <Check />,
+	warning: <Warning />,
 };
 
 export default toasterIcons;
