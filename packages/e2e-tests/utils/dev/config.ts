484bfd22c7aafb62b54608e3cfbe637375820a11
majidabbasi96 <majid.abbasi.56@gmail.com>
2022-08-25 18:14:57 +0400

    added a flag to support editor in ES

--- packages/e2e-tests/utils/dev/config.ts
+++ packages/e2e-tests/utils/dev/config.ts
@@ -1,13 +1,14 @@
 const commandArguments = process.argv;
 
 let IS_WP_MULTISITE_NETWORK = commandArguments.includes("-wp-multisite-network") ? true : false;
+let DO_NOT_USE_BARISTA_STRUCTURE = commandArguments.includes("-do-not-use-barista-structure") ? true : false;
 
 let WP_ADMIN_USER = { username: '', password: '' };
 let WP_BASE_URL = '';
 
 if(IS_WP_MULTISITE_NETWORK){
     WP_ADMIN_USER = { username: 'developers', password: '4(FuGWH7)!DqLVF(D9)F*4$5' };
-    WP_BASE_URL = 'https://e2etests.makeeventsnotwar.com'
+    WP_BASE_URL = 'http://e2etests.makeeventsnotwar.com'
 }else{
     WP_ADMIN_USER = { username: 'admin', password: 'password' };
     WP_BASE_URL = process.env.WP_BASE_URL || 'http://localhost:8889';
@@ -15,4 +16,4 @@ if(IS_WP_MULTISITE_NETWORK){
 
 const { WP_USERNAME = WP_ADMIN_USER.username, WP_PASSWORD = WP_ADMIN_USER.password } = process.env;
 
-export { WP_ADMIN_USER, WP_USERNAME, WP_PASSWORD, WP_BASE_URL, IS_WP_MULTISITE_NETWORK };
+export { WP_ADMIN_USER, WP_USERNAME, WP_PASSWORD, WP_BASE_URL, IS_WP_MULTISITE_NETWORK, DO_NOT_USE_BARISTA_STRUCTURE };
