16f7df79988f64be439f667aae5d26d8387c45b5
redblood639 <albinorabbithunter@gmail.com>
2023-04-17 19:54:54 -0400

    fix : comment and editor config

--- domains/core/admin/eventEditor/src/ui/tickets/ticketForm/multiStep/useOnSubmit.ts
+++ domains/core/admin/eventEditor/src/ui/tickets/ticketForm/multiStep/useOnSubmit.ts
@@ -8,14 +8,15 @@ import { OnSubmit } from './types';
 
 const useOnSubmit = (onClose: VoidFunction): OnSubmit => {
 	const mutateTicket = useMutateTicket();
-	const getCappedQuantity = useCappedQuantity();
 
+	const getCappedQuantity = useCappedQuantity();
 	const onSubmit = useCallback(
 		async (fields) => {
+			// wait the next event cycle to fire up isLoading for submit button
+			await wait();
+			// close the modal
+			onClose();
 			//  get the capped quantity for ticket based on the related date(s)
-			await wait(); // wait the next event cycle to fire up isLoading for submit button
-			onClose(); // close the modal
-
 			const quantity = getCappedQuantity({ quantity: fields.quantity, relatedDateIds: fields.datetimes });
 
 			const input = {
