eb99e8ef01ec7893bcd6e73bb6f8387f7275c8b2
tn3rb <brent@eventespresso.com>
2023-04-30 16:23:12 -0700

    strip TZ from formats; set time interval to 15mins

--- packages/dates/src/components/TimePicker.tsx
+++ packages/dates/src/components/TimePicker.tsx
@@ -1,17 +1,21 @@
 import { __ } from '@eventespresso/i18n';
-import { DEFAULT_TIME_FORMAT } from '@eventespresso/constants';
 
 import { DatePicker } from './DatePicker';
+
+import { DEFAULT_TIME_FORMAT } from '../constants';
+import { stripTimezoneFormat } from '../../../predicates';
 import type { DatePickerProps } from '../types';
 
 export const TimePicker: React.FC<DatePickerProps> = (props) => {
+	const timeFormat = stripTimezoneFormat(props?.timeFormat ?? DEFAULT_TIME_FORMAT);
 	return (
 		<DatePicker
 			calendarClassName='ee-timepicker'
-			dateFormat={props.timeFormat || DEFAULT_TIME_FORMAT}
+			dateFormat={timeFormat}
 			showTimeSelect
 			showTimeSelectOnly
 			timeCaption={__('time')}
+			timeFormat={timeFormat}
 			timeIntervals={15}
 			{...props}
 		/>
