2d0d672accd81ad349184f24145be819682a94a2
tn3rb <brent@eventespresso.com>
2023-04-30 16:10:07 -0700

    add stripTimezoneFormat()

--- packages/predicates/src/datetimes/index.ts
+++ packages/predicates/src/datetimes/index.ts
@@ -1,21 +1,20 @@
 export { default as capacityAtOrAbove } from './capacityAtOrAbove';
+export { default as isActive } from './isActive';
+export { default as isDateSoldOut } from './isSoldOut';
+export { default as isInMonth } from './isInMonth';
+export { default as isInYear } from './isInYear';
 export { default as isRecentlyExpired } from './isRecentlyExpired';
+export { default as isSoldOut } from './isSoldOut';
+export { default as isUpcoming } from './isUpcoming';
+export { default as sortDates } from './sorters';
 export { default as validFiniteCapacityLimit } from './validFiniteCapacityLimit';
 export { default as validSold } from './validSold';
 export { default as validStatus } from './validStatus';
 
-export { default as isActive } from './isActive';
-export { default as isInMonth } from './isInMonth';
-export { default as isInYear } from './isInYear';
-export { default as isDateSoldOut } from './isSoldOut';
-export { default as isUpcoming } from './isUpcoming';
-export { default as isSoldOut } from './isSoldOut';
-
-export { default as sortDates } from './sorters';
-
 export * from './constants';
-export * from './types';
-export * from './filters';
 export * from './datetimeFields';
+export * from './filters';
 export * from './selectionPredicates';
+export * from './stripTimezoneFormat';
+export * from './types';
 export * from './updatePredicates';
