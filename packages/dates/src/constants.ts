fb17de993765c98970316f3c2aaf0432dc5edc56
tn3rb <brent@eventespresso.com>
2023-04-30 16:19:33 -0700

    add DEFAULT_DATE_FORMAT & DEFAULT_TIME_FORMAT

--- packages/dates/src/constants.ts
+++ packages/dates/src/constants.ts
@@ -1,6 +1,8 @@
 import { __ } from '@eventespresso/i18n';
 
+export const DEFAULT_DATE_FORMAT = 'MMM d, yyyy'; // Aug 19, 2020
 export const DEFAULT_DATETIME_FORMAT = 'MMM d, yyyy h:mm aa'; // Aug 19, 2020 3:11 PM
+export const DEFAULT_TIME_FORMAT = 'h:mm aa'; // 3:11 PM
 
 export const endDateAfterStartDateErrorMessage = __('End Date & Time must be set later than the Start Date & Time');
 
