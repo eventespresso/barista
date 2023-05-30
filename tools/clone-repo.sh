aeba6e81028b49d12c94032164406a637454481f
tn3rb <brent@eventespresso.com>
2023-05-30 15:51:12 -0700

    update workflow paths

--- tools/clone-repo.sh
+++ tools/clone-repo.sh
@@ -6,7 +6,7 @@
 # | - | ----------- | ------------------------------------------- | ------------------------- |
 # | 1 | BRANCH      | target repo branch to checkout              | DEV                       |
 # | 2 | ORG         | organization of the target repo             | eventespresso             |
-# | 3 | REPO        | target repository name                      | cafe                      |
+# | 3 | REPO_NAME   | target repository name                      | cafe                      |
 # | - | ----------- | ------------------------------------------- | ------------------------- |
 
 set -e
@@ -16,6 +16,8 @@ CYAN='\033[0;36m'
 RED='\033[0;31m'
 RESET='\033[0m'
 
+BASE_DIR=$(dirname "$GITHUB_WORKSPACE")
+
 # The target repo branch to checkout during clone
 BRANCH="${1:-DEV}"
 # Convert "refs/heads/DEV" to "DEV"
@@ -23,7 +25,8 @@ BRANCH="${BRANCH#refs/heads/}"
 # GitHub account organization
 ORG="${2:-eventespresso}"
 # name of the repo e.g. "event-espresso-core"
-REPO=${3:-cafe}
+REPO_NAME=${3:-cafe}
+REPO_DIR="${BASE_DIR:?}/$REPO_NAME"
 
 if [[ -z "$BRANCH" ]]; then
 	printf "\n%b BRANCH is undefined%b\n" "$RED" "$RESET"
@@ -35,17 +38,17 @@ if [[ -z "$ORG" ]]; then
 	exit 1
 fi
 
-if [[ -z "$REPO" ]]; then
-	printf "\n%b REPO is undefined%b\n" "$RED" "$RESET"
+if [[ -z "$REPO_NAME" ]]; then
+	printf "\n%b REPO_NAME is undefined%b\n" "$RED" "$RESET"
 	exit 1
 fi
 
-printf "\n%bcloning repo: %s%b\n" "$CYAN" "$REPO" "$RESET"
+printf "\n%bcloning repo: %s%b\n" "$CYAN" "$REPO_NAME" "$RESET"
 # make sure the directory is empty
-rm -rf "$REPO"
+rm -rf "$REPO_DIR"
 
 # clone the repo branch
-git clone -b "$BRANCH" "git@github.com:$ORG/$REPO.git"
+git clone -b "$BRANCH" "git@github.com:$ORG/$REPO_NAME.git"
 
-printf "\n%bchanging directory: %s%b\n" "$CYAN" "$REPO" "$RESET"
-cd "$REPO"
+printf "\n%bchanging directory: %s%b\n" "$CYAN" "$REPO_DIR" "$RESET"
+cd "$REPO_DIR"
--- tools/copy-changes.sh
+++ tools/copy-changes.sh
@@ -6,7 +6,7 @@
 # | 2 | ASSETS_PATH | path to assets folder on the target repo    | assets                    |
 # | 3 | I18N_PATH   | path to languages folder on the target repo | languages                 |
 # | 4 | BUILD_PATH  | build path on the current/this repo         | build                     |
-# | 1 | REPO        | target repository name                      | cafe                      |
+# | 1 | REPO_DIR    | path to target repository                   | cafe                      |
 # | - | ----------- | ------------------------------------------- | ------------------------- |
 
 set -e
@@ -18,17 +18,21 @@ RESET='\033[0m'
 
 CURRENT_DIR=$(pwd)
 
+BASE_DIR=$(dirname "$GITHUB_WORKSPACE")
+
 # Default path to assets folder (on target repo)
 ASSETS_PATH="${1:-assets}"
 # Default path to languages folder (on target repo)
 I18N_PATH="${2:-languages}"
 # Default path to build folder
 BUILD_PATH="${3:-build}"
-# name of the repo e.g. "event-espresso-core"
-REPO=${4:-cafe}
+BUILD_PATH="${GITHUB_WORKSPACE:?}/$BUILD_PATH"
+# path to the repo e.g. "/home/runner/work/cafe/cafe"
+REPO_DIR=${4:-cafe}
+REPO_DIR="${BASE_DIR:?}/$REPO_DIR"
 
-if [[ -z "$REPO" ]]; then
-	printf "\n%b REPO is undefined%b\n" "$RED" "$RESET"
+if [[ -z "$REPO_DIR" ]]; then
+	printf "\n%b REPO_DIR is undefined%b\n" "$RED" "$RESET"
 	exit 1
 fi
 if [[ -z "$ASSETS_PATH" ]]; then
@@ -44,19 +48,20 @@ if [[ -z "$BUILD_PATH" ]]; then
 	exit 1
 fi
 
-printf "\n%bcurrent directory: %s%b" "$CYAN" "$CURRENT_DIR" "$RESET"
-# printf "\n%bchanging directory: %s%b" "$CYAN" "$REPO" "$RESET"
-# cd "$REPO"
+printf "\n%bBUILD_PATH: %s%b" "$CYAN" "$BUILD_PATH" "$RESET"
+printf "\n%bREPO_DIR: %s%b" "$CYAN" "$REPO_DIR" "$RESET"
+# printf "\n%bchanging directory: %s%b" "$CYAN" "$REPO_DIR" "$RESET"
+# cd "$REPO_DIR"
 
 ## DEPLOY I18N ##
 # paths of the translation files
-PHP_I18N_FILE="$REPO/$I18N_PATH/event_espresso-translations-js.php"
-JS_I18N_FILE="$CURRENT_DIR/$BUILD_PATH/js-translations.pot"
+PHP_I18N_FILE="$REPO_DIR/$I18N_PATH/event_espresso-translations-js.php"
+JS_I18N_FILE="$BUILD_PATH/js-translations.pot"
 
 # If DEPLOY_I18N is not set to "no"
 if [ "$DEPLOY_I18N" != "no" ]; then
 	## make sure languages directory exists
-	mkdir -p "$REPO/$I18N_PATH"
+	mkdir -p "$REPO_DIR/$I18N_PATH"
 	# make sure the file exists
 	touch "$PHP_I18N_FILE"
 	# Convert POT file to PHP
@@ -72,7 +77,7 @@ if [ "$REMOVE_JS_I18N_FILE" != "no" ]; then
 fi
 
 # # goto the repo directory
-# cd "$REPO"
+# cd "$REPO_DIR"
 
 # If DEPLOY_ASSETS is not set to "no"
 if [ "$DEPLOY_ASSETS" != "no" ]; then
@@ -85,6 +90,7 @@ if [ "$DEPLOY_ASSETS" != "no" ]; then
 	mkdir -p "$ASSETS_PATH"
 
 	# copy files from build folder to target assets folder
+	# cp -r /var/www/cafe.test/wp-content/plugins/barista/build/* /var/www/cafe.test/wp-content/plugins/event-espresso-core/assets/
 	printf "\n%copy build files to assets path...%b\n" "$CYAN" "$RESET"
 	cp -r "$CURRENT_DIR/$BUILD_PATH"/* "$ASSETS_PATH"/
 fi
