db5b196c6bdcdec71de6c88f6fcf42b7b9373da4
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-06-19 17:29:48 +0300

    Create utility function modifyDate()

---
+++ packages/dates/src/utils/modifyDate.ts
@@ -0,0 +1,102 @@
+import {
+	addDays,
+	addHours,
+	addMilliseconds,
+	addMinutes,
+	addMonths,
+	addQuarters,
+	addSeconds,
+	addWeeks,
+	addYears,
+	subDays,
+	subHours,
+	subMilliseconds,
+	subMinutes,
+	subMonths,
+	subQuarters,
+	subSeconds,
+	subWeeks,
+	subYears,
+} from 'date-fns';
+import { IntervalType, ShiftDateArgs } from './types';
+
+type DateFnKey = Exclude<IntervalType, 'ISOWeekYears'>;
+
+type DateFn = (date: Date | number, amount: number) => Date;
+
+type modifiers = {
+	add: Record<DateFnKey, DateFn>;
+	sub: Record<DateFnKey, DateFn>;
+};
+
+const modifiers: modifiers = {
+	add: {
+		milliseconds: addMilliseconds,
+		seconds: addSeconds,
+		minutes: addMinutes,
+		hours: addHours,
+		days: addDays,
+		weeks: addWeeks,
+		months: addMonths,
+		quarters: addQuarters,
+		years: addYears,
+	},
+	sub: {
+		milliseconds: subMilliseconds,
+		seconds: subSeconds,
+		minutes: subMinutes,
+		hours: subHours,
+		days: subDays,
+		weeks: subWeeks,
+		months: subMonths,
+		quarters: subQuarters,
+		years: subYears,
+	},
+};
+
+type modifyDate = { date: Date } & ShiftDateArgs;
+
+const modifyDate = ({ date, unit, value, type }: modifyDate): Date => {
+	const wholeValue = Math.floor(value);
+
+	const modulo = value % 1;
+
+	const wholeFn = getDateFn(type, unit);
+
+	const wholeDate = wholeFn(date, wholeValue);
+
+	// there are no decimal points, just return integer
+	if (modulo === 0) return wholeDate;
+
+	// there are decimal points, continue here
+
+	const frValue = Math.round(modulo * 60);
+
+	const frUnit = getPreviousUnit(unit);
+
+	const frFn = getDateFn(type, frUnit);
+
+	return frFn(wholeDate, frValue);
+};
+
+const getPreviousUnit = (unit: IntervalType): IntervalType => {
+	// modifiers.add and modifiers.sub are symmetrical
+	// https://stackoverflow.com/questions/52856496/typescript-object-keys-return-string
+	const keys = Array.from(Object.keys(modifiers.add)) as IntervalType[];
+	const i = keys.findIndex((v) => v === unit);
+	if (i === -1) return unit; // unit not found, keep unit as is
+	if (i === 0) return unit; // smallest possible unit
+	return keys[i - 1];
+};
+
+const getDateFn = (type: ShiftDateArgs['type'], unit: IntervalType): DateFn => {
+	const oKey: keyof modifiers = type === 'earlier' ? 'sub' : 'add';
+
+	if (!(unit in modifiers[oKey])) throw new Error('Unexpected condition');
+
+	const iKey = unit as keyof modifiers[keyof modifiers]; // inner key
+
+	return modifiers[oKey][iKey];
+};
+
+export { modifyDate };
