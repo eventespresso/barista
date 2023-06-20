ed68f68416887ac056a119c5f736c862ac02e84e
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-06-20 20:41:21 +0300

    Add work-in-progress unit test for modifyDate() function

---
+++ packages/dates/src/utils/test/modifyDate/fixture.ts
@@ -0,0 +1,221 @@
+import { modifyDateProps } from '../../modifyDate';
+
+// https://jestjs.io/docs/api#testeachtablename-fn-timeout
+type Table = { expected: Date } & modifyDateProps;
+
+const addInteger: Table[] = [
+	{
+		date: new Date(2023, 0, 1, 0, 0, 0, 0),
+		unit: 'milliseconds',
+		value: 100,
+		type: 'later',
+		expected: new Date(2023, 0, 1, 0, 0, 0, 100),
+	},
+	{
+		date: new Date(2023, 0, 1, 0, 0, 0, 0),
+		unit: 'seconds',
+		value: 35,
+		type: 'later',
+		expected: new Date(2023, 0, 1, 0, 0, 35, 0),
+	},
+	{
+		date: new Date(2023, 0, 1, 0, 0, 0, 0),
+		unit: 'minutes',
+		value: 7,
+		type: 'later',
+		expected: new Date(2023, 0, 1, 0, 7, 0, 0),
+	},
+	{
+		date: new Date(2023, 0, 1, 0, 0, 0, 0),
+		unit: 'hours',
+		value: 10,
+		type: 'later',
+		expected: new Date(2023, 0, 1, 10, 0, 0, 0),
+	},
+	{
+		date: new Date(2023, 0, 1, 0, 0, 0, 0),
+		unit: 'days',
+		value: 5,
+		type: 'later',
+		expected: new Date(2023, 0, 6, 0, 0, 0, 0),
+	},
+	{
+		date: new Date(2023, 0, 1, 0, 0, 0, 0),
+		unit: 'weeks',
+		value: 3,
+		type: 'later',
+		expected: new Date(2023, 0, 22, 0, 0, 0, 0),
+	},
+	{
+		date: new Date(2023, 0, 1, 0, 0, 0, 0),
+		unit: 'months',
+		value: 2,
+		type: 'later',
+		expected: new Date(2023, 2, 1, 0, 0, 0, 0),
+	},
+	{
+		date: new Date(2023, 0, 1, 0, 0, 0, 0),
+		unit: 'quarters',
+		value: 3,
+		type: 'later',
+		expected: new Date(2023, 9, 1, 0, 0, 0, 0),
+	},
+	{
+		date: new Date(2023, 0, 1, 0, 0, 0, 0),
+		unit: 'years',
+		value: 1,
+		type: 'later',
+		expected: new Date(2024, 0, 1, 0, 0, 0, 0),
+	},
+];
+
+const subInteger: Table[] = [
+	{
+		date: new Date(2023, 0, 1, 0, 0, 0, 100),
+		unit: 'milliseconds',
+		value: 100,
+		type: 'earlier',
+		expected: new Date(2023, 0, 1, 0, 0, 0, 0),
+	},
+	{
+		date: new Date(2023, 0, 1, 0, 0, 35, 0),
+		unit: 'seconds',
+		value: 35,
+		type: 'earlier',
+		expected: new Date(2023, 0, 1, 0, 0, 0, 0),
+	},
+	{
+		date: new Date(2023, 0, 1, 0, 7, 0, 0),
+		unit: 'minutes',
+		value: 7,
+		type: 'earlier',
+		expected: new Date(2023, 0, 1, 0, 0, 0, 0),
+	},
+	{
+		date: new Date(2023, 0, 1, 10, 0, 0, 0),
+		unit: 'hours',
+		value: 10,
+		type: 'earlier',
+		expected: new Date(2023, 0, 1, 0, 0, 0, 0),
+	},
+	{
+		date: new Date(2023, 0, 6, 0, 0, 0, 0),
+		unit: 'days',
+		value: 5,
+		type: 'earlier',
+		expected: new Date(2023, 0, 1, 0, 0, 0, 0),
+	},
+	{
+		date: new Date(2023, 0, 22, 0, 0, 0, 0),
+		unit: 'weeks',
+		value: 3,
+		type: 'earlier',
+		expected: new Date(2023, 0, 1, 0, 0, 0, 0),
+	},
+	{
+		date: new Date(2023, 2, 1, 0, 0, 0, 0),
+		unit: 'months',
+		value: 2,
+		type: 'earlier',
+		expected: new Date(2023, 0, 1, 0, 0, 0, 0),
+	},
+	{
+		date: new Date(2023, 9, 1, 0, 0, 0, 0),
+		unit: 'quarters',
+		value: 3,
+		type: 'earlier',
+		expected: new Date(2023, 0, 1, 0, 0, 0, 0),
+	},
+	{
+		date: new Date(2024, 0, 1, 0, 0, 0, 0),
+		unit: 'years',
+		value: 1,
+		type: 'earlier',
+		expected: new Date(2023, 0, 1, 0, 0, 0, 0),
+	},
+];
+
+const addFraction: Table[] = [
+	{
+		date: new Date(2023, 0, 1, 0, 0, 0, 0),
+		unit: 'milliseconds',
+		value: 100.5,
+		type: 'later',
+		expected: new Date(2023, 0, 1, 0, 0, 0, 101),
+	},
+	{
+		date: new Date(2023, 0, 1, 0, 0, 0, 0),
+		unit: 'seconds',
+		value: 35.5,
+		type: 'later',
+		expected: new Date(2023, 0, 1, 0, 0, 35, 500),
+	},
+	{
+		date: new Date(2023, 0, 1, 0, 0, 0, 0),
+		unit: 'minutes',
+		value: 7.5,
+		type: 'later',
+		expected: new Date(2023, 0, 1, 0, 7, 30, 0),
+	},
+	{
+		date: new Date(2023, 0, 1, 0, 0, 0, 0),
+		unit: 'hours',
+		value: 10.66,
+		type: 'later',
+		expected: new Date(2023, 0, 1, 10, 39, 36, 0),
+	},
+	{
+		date: new Date(2023, 0, 1, 0, 0, 0, 0),
+		unit: 'days',
+		value: 5.23,
+		type: 'later',
+		expected: new Date(2023, 0, 6, 5, 31, 12, 0),
+	},
+	// TODO:
+	// {
+	// 	date: new Date(2023, 0, 1, 0, 0, 0, 0),
+	// 	unit: 'weeks',
+	// 	value: 3,
+	// 	type: 'later',
+	// 	expected: new Date(2023, 0, 22, 0, 0, 0, 0),
+	// },
+	// TODO:
+	// {
+	// 	date: new Date(2023, 0, 1, 0, 0, 0, 0),
+	// 	unit: 'months',
+	// 	value: 2,
+	// 	type: 'later',
+	// 	expected: new Date(2023, 2, 1, 0, 0, 0, 0),
+	// },
+	// TODO:
+	// {
+	// 	date: new Date(2023, 0, 1, 0, 0, 0, 0),
+	// 	unit: 'quarters',
+	// 	value: 3,
+	// 	type: 'later',
+	// 	expected: new Date(2023, 9, 1, 0, 0, 0, 0),
+	// },
+	// TODO:
+	// {
+	// 	date: new Date(2023, 0, 1, 0, 0, 0, 0),
+	// 	unit: 'years',
+	// 	value: 1,
+	// 	type: 'later',
+	// 	expected: new Date(2024, 0, 1, 0, 0, 0, 0),
+	// },
+];
+
+const subFraction = {};
+
+const fixture = {
+	add: {
+		integer: addInteger,
+		fraction: addFraction,
+	},
+	sub: {
+		integer: subInteger,
+		fraction: subFraction,
+	},
+};
+
+export { fixture };
