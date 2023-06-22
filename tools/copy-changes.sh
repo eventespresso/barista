40693ed6a0d86eda767c84b2359b0073153afd6f
tn3rb <brent@eventespresso.com>
2023-06-22 11:03:36 -0700

    fix paths in copy-changes.sh

--- tools/copy-changes.sh
+++ tools/copy-changes.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# | - | ----------- | ------------------------------------------- | ------------------------- |
-# | # | VAR NAME    | DESCRIPTION                                 | DEFAULT                   |
-# | - | ----------- | ------------------------------------------- | ------------------------- |
-# | 1 | ASSETS_PATH | path to assets folder on the target repo    | assets                    |
-# | 2 | I18N_PATH   | path to languages folder on the target repo | languages                 |
-# | 3 | BUILD_PATH  | build path on the current/this repo         | build                     |
-# | 4 | REPO_DIR    | path to target repository                   | cafe                      |
-# | - | ----------- | ------------------------------------------- | ------------------------- |
+# | - | ------------------- | ------------------------------------------- | ------------------------- |
+# | # | VAR NAME            | DESCRIPTION                                 | DEFAULT                   |
+# | - | ------------------- | ------------------------------------------- | ------------------------- |
+# | 1 | ASSETS_PATH         | path to assets folder on the target repo    | assets                    |
+# | 2 | I18N_PATH           | path to languages folder on the target repo | languages                 |
+# | 3 | BUILD_PATH          | build path on the current/this repo         | build                     |
+# | 4 | REPO_DIR            | path to target repository                   | cafe                      |
+# | 5 | REMOVE_JS_I18N_FILE | delete JS pot file?              			  | yes                       |
+# | 6 | DEPLOY_ASSETS       | copy build files to target assets folder    | yes                       |
+# | - | ------------------- | ------------------------------------------- | ------------------------- |
 
 set -e
 
@@ -16,20 +18,24 @@ CYAN='\033[0;36m'
 RED='\033[0;31m'
 RESET='\033[0m'
 
-CURRENT_DIR=$(pwd)
-
 BASE_DIR=$(dirname "$GITHUB_WORKSPACE")
 
 # Default path to assets folder (on target repo)
 ASSETS_PATH="${1:-assets}"
+
 # Default path to languages folder (on target repo)
 I18N_PATH="${2:-languages}"
+
 # Default path to build folder
 BUILD_PATH="${3:-build}"
 BUILD_PATH="${GITHUB_WORKSPACE:?}/$BUILD_PATH"
-# path to the repo e.g. "/home/runner/work/cafe/cafe"
+
+# path to the repo ex: "/home/runner/work/barista/cafe"
 REPO_DIR=${4:-cafe}
 REPO_DIR="${BASE_DIR:?}/$REPO_DIR"
+
+REMOVE_JS_I18N_FILE=${5:-yes}
+DEPLOY_ASSETS=${6:-yes}
 
 if [[ -z "$REPO_DIR" ]]; then
 	printf "\n%b REPO_DIR is undefined%b\n" "$RED" "$RESET"
@@ -82,15 +88,16 @@ fi
 # If DEPLOY_ASSETS is not set to "no"
 if [ "$DEPLOY_ASSETS" != "no" ]; then
 	# clean the assets path.
-	printf "\n%cleaning up assets path...%b\n" "$CYAN" "$RESET"
-	rm -rf "$ASSETS_PATH"/static/*
-	rm -f "$ASSETS_PATH"/asset-manifest.json
+	printf "\n%bcleaning up assets path...%b\n" "$CYAN" "$RESET"
+	rm -rf "$REPO_DIR/$ASSETS_PATH"/static/*
+	rm -f "$REPO_DIR/$ASSETS_PATH"/asset-manifest.json
 
 	# Make sure the directory exists
-	mkdir -p "$ASSETS_PATH"
+	mkdir -p "$REPO_DIR/$ASSETS_PATH"
 
 	# copy files from build folder to target assets folder
-	# cp -r /var/www/cafe.test/wp-content/plugins/barista/build/* /var/www/cafe.test/wp-content/plugins/event-espresso-core/assets/
-	printf "\n%copy build files to assets path...%b\n" "$CYAN" "$RESET"
-	cp -r "$CURRENT_DIR/$BUILD_PATH"/* "$ASSETS_PATH"/
+	# ex: cp -r /home/runner/work/barista/cafe/wp-content/plugins/barista/build/*
+	# 			/home/runner/work/barista/cafe/wp-content/plugins/event-espresso-core/assets/
+	printf "\n%bcopy build files to assets path...%b\n" "$CYAN" "$RESET"
+	cp -r "$BUILD_PATH"/* "$REPO_DIR/$ASSETS_PATH/"
 fi
