5dc000283ad72305cfd034712faf19b2f724d9fc
tn3rb <brent@eventespresso.com>
2023-06-23 16:21:13 -0700

    refactor ErrorIndicator using Banner

--- packages/ui-components/src/Banner/ErrorIndicator/index.tsx
+++ packages/ui-components/src/Banner/ErrorIndicator/index.tsx
@@ -1,16 +1,6 @@
 import { Banner } from '../';
-import { WarningTriangle } from '@eventespresso/icons';
-import type { ErrorIndicatorProps } from './types';
+import type { BannerProps } from '../types';
 
-import './style.scss';
-
-export const ErrorIndicator: React.FC<ErrorIndicatorProps> = ({ description, title }) => (
-	<Banner
-		className='ee-error-indicator'
-		description={description}
-		icon={<WarningTriangle />}
-		status='error'
-		title={title}
-		variant='subtle'
-	/>
+export const ErrorIndicator: React.FC<BannerProps> = ({ message, title }) => (
+	<Banner className='ee-error-indicator' message={message} status='error' title={title} variant='subtle' />
 );
