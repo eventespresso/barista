fb5061058bdf89de04b02823d10aeead4a617950
tn3rb <brent@eventespresso.com>
2023-06-23 17:57:34 -0700

    ensure correct branch is checked out

--- tools/create-branch.sh
+++ tools/create-branch.sh
@@ -4,6 +4,8 @@
 # | # | VAR NAME    | DESCRIPTION                                 | DEFAULT                   |
 # | - | ----------- | ------------------------------------------- | ------------------------- |
 # | 1 | BRANCH      | name of new branch to create                |                           |
+# | 2 | BASE_BRANCH | base branch for new branch in target repo   | DEV                       |
+# | 3 | REPO_NAME   | target repository name                      | cafe                      |
 # | - | ----------- | ------------------------------------------- | ------------------------- |
 
 set -e
@@ -13,16 +15,30 @@ CYAN='\033[0;36m'
 RED='\033[0;31m'
 RESET='\033[0m'
 
+BASE_DIR=$(dirname "$GITHUB_WORKSPACE")
+
 # name of new branch to create
 BRANCH="$1"
+# Convert "refs/heads/dev" to "dev"
+BRANCH="${BRANCH#refs/heads/}"
+
+# The target repo branch to base new branch off of
+BASE_BRANCH="${2:-DEV}"
+# Convert "refs/heads/dev" to "dev"
+BASE_BRANCH="${BASE_BRANCH#refs/heads/}"
+
+# name of the repo e.g. "event-espresso-core"
+REPO_NAME=${3:-cafe}
+REPO_DIR="${BASE_DIR:?}/$REPO_NAME"
 
 if [[ -z "$BRANCH" ]]; then
 	printf "\n%b BRANCH is undefined%b\n" "$RED" "$RESET"
 	exit 1
 fi
 
-# Convert "refs/heads/dev" to "dev"
-BRANCH="${BRANCH#refs/heads/}"
+printf "\n%bchanging directory: %s%b\n" "$CYAN" "$REPO_DIR" "$RESET"
+cd "$REPO_DIR"
+git checkout "$BASE_BRANCH"
 
 printf "\n%b creating branch %s%b\n" "$CYAN" "$BRANCH" "$RESET"
 git branch "$BRANCH"
