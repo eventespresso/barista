e3a22a2ee8ef14d7a198f0914833331607d20b3c
tn3rb <brent@eventespresso.com>
2023-06-23 17:00:43 -0700

    fix paths in commit-changes.sh

--- tools/commit-changes.sh
+++ tools/commit-changes.sh
@@ -5,7 +5,7 @@
 # | - | ----------- | ------------------------------------------- | ------------------------- |
 # | 1 | BRANCH      | new branch to commit changes to             |                           |
 # | 3 | BASE_BRANCH | branch to deploy at, in the target repo     | DEV                       |
-# | 2 | REPO        | target repository name                      | cafe                      |
+# | 2 | REPO_NAME   | target repository name                      | cafe                      |
 # | - | ----------- | ------------------------------------------- | ------------------------- |
 
 # DEFAULT ENV VARIABLES SET BY GITHUB
@@ -20,6 +20,8 @@ CYAN='\033[0;36m'
 RED='\033[0;31m'
 RESET='\033[0m'
 
+BASE_DIR=$(dirname "$GITHUB_WORKSPACE")
+
 # nameof new branch to commit changes to
 BRANCH="$1"
 # Convert "refs/heads/dev" to "dev"
@@ -29,15 +31,16 @@ BASE_BRANCH="${2:-DEV}"
 # Convert "refs/heads/dev" to "dev"
 BASE_BRANCH="${BASE_BRANCH#refs/heads/}"
 # name of the repo e.g. "event-espresso-core"
-REPO=${3:-cafe}
+REPO_NAME=${3:-cafe}
+REPO_DIR="${BASE_DIR:?}/$REPO_NAME"
 
 if [[ -z "$BRANCH" ]]; then
 	printf "\n%b BRANCH is undefined%b\n" "$RED" "$RESET"
 	exit 1
 fi
 
-if [[ -z "$REPO" ]]; then
-	printf "\n%b REPO is undefined%b\n" "$RED" "$RESET"
+if [[ -z "$REPO_NAME" ]]; then
+	printf "\n%b REPO_NAME is undefined%b\n" "$RED" "$RESET"
 	exit 1
 fi
 
@@ -49,18 +52,18 @@ fi
 # the source commit SHA with repo URL
 SOURCE_COMMIT="${GITHUB_REPOSITORY}@${GITHUB_SHA}"
 
-printf "\n%bchanging directory: %s%b\n" "$CYAN" "$REPO" "$RESET"
-cd "$REPO"
+printf "\n%bchanging directory: %s%b\n" "$CYAN" "$REPO_DIR" "$RESET"
+cd "$REPO_DIR"
 
 # Commit if there is anything to
 if [ -n "$(git status --porcelain)" ]; then
-	printf "\n%bcommitting changes to: %s%b\n" "$CYAN" "$REPO" "$RESET"
+	printf "\n%bcommitting changes to: %s%b\n" "$CYAN" "$REPO_NAME" "$RESET"
 	# Stage all files
 	git add .
 	# add source commit to track the deployment source
 	git commit --message "Deployed from $SOURCE_COMMIT"
 
-	printf "\n%pushing %s branch to %s repo%b\n" "$CYAN" "$BRANCH" "$REPO" "$RESET"
+	printf "\n%pushing %s branch to %s repo%b\n" "$CYAN" "$BRANCH" "$REPO_NAME" "$RESET"
 	git push --set-upstream origin "$BRANCH"
 else
 	printf "\n%bNo changes%b\n" "$CYAN" "$RESET"
