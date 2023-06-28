0645fd74ed1d1cac1afa38be9858c0b8d4a496f9
tn3rb <brent@eventespresso.com>
2023-06-27 18:48:36 -0700

    extract logic into BannerIcon component

--- packages/ui-components/src/Banner/Banner.tsx
+++ packages/ui-components/src/Banner/Banner.tsx
@@ -1,35 +1,13 @@
 import classNames from 'classnames';
 
 import { Banner as BannerAdapter } from '@eventespresso/adapters';
-import { Check, ExclamationCircleFilled, InfoCircleFilled, WarningTriangle } from '@eventespresso/icons';
+import { BannerIcon } from './BannerIcon';
 import type { BannerProps } from './types';
 
 import './style.scss';
 
 export const Banner: React.FC<BannerProps> = ({ message, icon, iconProps, status, title, variant, ...props }) => {
 	const className = classNames('ee-banner', status && `ee-banner--${status}`, props.className);
-	let iconComp: React.ReactNode;
-	if (icon) {
-		iconComp = icon;
-	} else {
-		switch (status) {
-			case 'error':
-				iconComp = <ExclamationCircleFilled className={'ee-banner__icon'} {...iconProps} />;
-				break;
-			case 'info':
-				iconComp = <InfoCircleFilled className={'ee-banner__icon'} {...iconProps} />;
-				break;
-			case 'success':
-				iconComp = <Check className={'ee-banner__icon'} {...iconProps} />;
-				break;
-			case 'warning':
-				iconComp = <WarningTriangle className={'ee-banner__icon'} {...iconProps} />;
-				break;
-			default:
-				iconComp = <InfoCircleFilled className={'ee-banner__icon'} {...iconProps} />;
-				break;
-		}
-	}
-
+	const iconComp = <BannerIcon icon={icon} iconProps={iconProps} status={status} />;
 	return <BannerAdapter className={className} message={message} icon={iconComp} title={title} variant={variant} />;
 };
