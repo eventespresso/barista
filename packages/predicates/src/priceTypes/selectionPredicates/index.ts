45f6772e81ed05a72131bd9aedc6670bf8564b95
tn3rb <brent@eventespresso.com>
2023-06-29 10:07:31 -0700

    add isPriceType() predicate

--- packages/predicates/src/priceTypes/selectionPredicates/index.ts
+++ packages/predicates/src/priceTypes/selectionPredicates/index.ts
@@ -4,6 +4,11 @@ import { isNotBasePrice, isNotDiscount, isNotPercent } from '../../prices';
 
 import type { PriceType } from '@eventespresso/edtr-services';
 
+// returns `true` if supplied object is of type `PriceType`
+export const isPriceType = (object: PriceType): object is PriceType => {
+	return 'baseType' in object;
+};
+
 // returns true if supplied price type is a flat fee (dollar) surcharge
 export const isFlatFeeSurcharge = allPass([isNotBasePrice, isNotDiscount, isNotPercent]);
 
