12f2f2ba727fe10192758db1711861e9efbd8d17
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-07-04 23:32:05 +0300

    Add web server to Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,6 +7,7 @@ import { defineConfig, devices } from '@playwright/test';
 // require('dotenv').config();
 
 const host = process.env.PLAYWRIGHT_HOST ?? 'http://localhost:8889';
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -70,9 +71,9 @@ export default defineConfig({
 	],
 
 	/* Run your local dev server before starting the tests */
-	// webServer: {
-	//   command: 'npm run start',
-	//   url: 'http://127.0.0.1:3000',
-	//   reuseExistingServer: !process.env.CI,
-	// },
+	webServer: {
+		command: 'yarn docker:start',
+		url: host,
+		reuseExistingServer: !process.env.CI,
+	},
 });
