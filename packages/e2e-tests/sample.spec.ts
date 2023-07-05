8a015c8052809eb6c05d41b50e1c7cfa4ddfa724
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-07-05 03:55:18 +0300

    Add E2E sample test that uses @playwright/test

---
+++ packages/e2e-tests/sample.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+	await page.goto('http://localhost:8889');
+
+	await page.waitForLoadState();
+
+	await expect(page).toHaveTitle(/barista/);
+});
+
+test('has h1 header', async ({ page }) => {
+	await page.goto('http://localhost:8889');
+
+	const h1 = page.locator('h1');
+
+	await h1.waitFor({ state: 'visible' });
+
+	expect(h1).toContainText('Mindblown: a blog about philosophy.');
+});
