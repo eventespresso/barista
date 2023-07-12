817a235ebeab3462fb0198c608cb7ecc2c8adb6e
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-07-12 05:14:59 +0300

    Do not clean e2e tests when in CI or DEBUG_E2E (env var)

--- e2e-tests/setup/global-teardown.ts
+++ e2e-tests/setup/global-teardown.ts
@@ -4,7 +4,7 @@ import { execSync } from 'child_process';
 
 async function globalTeardown() {
 	// do not cleanup in CI since it does not have state like local environment
-	if (!process.env.CI) {
+	if (!process.env.CI && !process.env.DEBUG_E2E) {
 		const root = resolve(__dirname, '..', '..', '.playwright');
 		if (existsSync(root)) {
 			rmSync(root, {
