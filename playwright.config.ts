68297b07d4fc2e12bd5bf702fc1dc37f6c7bd9eb
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-07-13 13:07:39 +0300

    Increase Playwright timeout per test to 120 seconds

--- playwright.config.ts
+++ playwright.config.ts
@@ -45,9 +45,6 @@ export default defineConfig({
 	/* Set global timeout to 1 hour */
 	globalTimeout: 3_600_000,
 
-	/* Set global timeout to 1 hour */
-	globalTimeout: 3_600_000,
-
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
