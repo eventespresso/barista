b044421252a1d95579c6d451f468da30a52d5660
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-07-18 20:10:58 +0300

    Create E2E fixture Events

--- e2e-tests/index.ts
+++ e2e-tests/index.ts
@@ -2,3 +2,4 @@ export { Auth } from './fixtures/Auth';
 export { Navigate } from './fixtures/Navigate';
 export { test } from './fixtures/fixtures';
 export { Nuke } from './fixtures/Nuke';
+export { Events } from './fixtures/Events';
