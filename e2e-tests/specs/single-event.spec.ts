be049c4a9420fa90fe7d93f04471a4c1b9616d13
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-07-20 23:24:01 +0300

    Create E2E stub for single Event page

---
+++ e2e-tests/specs/single-event.spec.ts
@@ -0,0 +1,19 @@
+import { test } from '@eventespresso/e2e';
+
+test.describe('pagination for Event Dates', () => {
+	test.fixme('card view', async () => {});
+	test.fixme('table view', async () => {});
+});
+
+test.describe('pagination for Available Tickets', () => {
+	test.fixme('card view', async () => {});
+	test.fixme('table view', async () => {});
+});
+
+test.describe('search functionality', () => {
+	test.fixme('basic search query', async () => {});
+	test.fixme('case insensitive search query', async () => {});
+	test.fixme('partial search query', async () => {});
+	test.fixme('non English search query', async () => {});
+	test.fixme('name and description', async () => {});
+});
