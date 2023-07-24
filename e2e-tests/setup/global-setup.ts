dc67b584005f50813ce6990e1f84a61709ea5e9d
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-07-24 23:28:07 +0300

    Enable permalinks for E2E tests

--- e2e-tests/setup/global-setup.ts
+++ e2e-tests/setup/global-setup.ts
@@ -2,6 +2,7 @@ import { execSync } from 'child_process';
 
 async function globalSetup() {
 	execSync('yarn docker:cli plugin activate event-espresso-core barista');
+	execSync(`yarn docker:cli permalinks enable '/%postname%/'`);
 }
 
 export default globalSetup;
