b2c3309bef5855c8fae95f70d303bb7619b09233
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-08-03 01:44:07 +0300

    Add support for PlayWright --project when using DDEV

--- e2e-tests/setup/global-setup.ts
+++ e2e-tests/setup/global-setup.ts
@@ -1,12 +1,28 @@
+import { argv } from 'process';
+import minimist from 'minimist';
 import playwright from '../playwright.config';
 import { constants, MakeConfig, MakeEnv } from '@eventespresso/e2e';
 
+if (!playwright.projects || playwright.projects.length < 1) {
+	throw new Error('You need at least one project with user agent in playwright.config.ts!');
+}
+
+// include only projects with defined name *and* user-agent
+const projects = playwright.projects.filter((project) => project.name && project.use?.userAgent);
+
+const args = minimist(argv.slice(2));
+
+// PlayWright supports --project switch to include only specific projects
+// otherwise include everything
+const whitelist = (args['project'] as string[]) ?? playwright.projects.map((project) => project.name);
+
+const factory = new MakeEnv(new MakeConfig());
+
 async function globalSetup() {
-	const factory = new MakeEnv(new MakeConfig());
-	const projects = playwright.projects ?? [];
 	for (const project of projects) {
-		if (project.use?.userAgent && project.name) {
-			await factory.make(project.name, constants.locations.manifests);
+		// see filter about where we make sure name *and* user-agent is included
+		if (whitelist.includes(project.name!)) {
+			await factory.make(project.name!, constants.locations.manifests);
 		}
 	}
 }
