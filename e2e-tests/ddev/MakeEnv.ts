a3ee5dfc3e9eefdda2bde7b9305439f92586a0c1
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-08-03 23:02:51 +0300

    Generate certficate for DDEV with root CA from the host

--- e2e-tests/ddev/MakeEnv.ts
+++ e2e-tests/ddev/MakeEnv.ts
@@ -5,6 +5,7 @@ import { MakeConfig } from './MakeConfig';
 import { Command } from '@commander-js/extra-typings';
 import dotenv from 'dotenv';
 import { Manifest } from '@eventespresso/e2e';
+import { ensureDirSync } from 'fs-extra';
 
 class MakeEnv {
 	constructor(private readonly config: MakeConfig) {}
@@ -18,9 +19,30 @@ class MakeEnv {
 		const repositories = { cafe: env['CAFE'] as string, barista: env['BARISTA'] as string };
 		const manifest = new Manifest(project);
 		await this.config.make(manifest, repositories);
+		await this.makeCertificate(manifest);
 		await this.startContainer(manifest);
 		manifest.url = this.getUrl(manifest);
 		manifest.save();
+	}
+
+	private async makeCertificate(manifest: Manifest): Promise<void> {
+		const cmd = this.makeMkcertCmd(manifest);
+		ensureDirSync(resolve(manifest.cwd, '.ddev/custom_certs'));
+		const mkcert = spawn(cmd, [], { cwd: manifest.cwd, shell: true, stdio: 'inherit' });
+		mkcert.on('message', (msg) => console.log(msg.toString));
+		mkcert.on('error', (err) => console.error(err.message));
+		// block further execution and wait until spawn finishes
+		// https://stackoverflow.com/a/69025854/4343719
+		await new Promise((resolve) => mkcert.on('close', resolve));
+	}
+
+	private makeMkcertCmd({ project, cwd }: Manifest): string {
+		const keyName = `${project}.key`;
+		const keyFile = resolve(cwd, '.ddev/custom_certs', keyName);
+		const certName = `${project}.pem`;
+		const certFile = resolve(cwd, '.ddev/custom_certs', certName);
+		const tld = `${project}.ddev.site`;
+		return `mkcert -key-file ${keyFile} -cert-file ${certFile} ${tld}`;
 	}
 
 	private async startContainer(manifest: Manifest): Promise<void> {
