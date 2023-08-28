bdf29e6ec727d162d93dcdecb4947e9a69c678ea
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-08-28 22:01:55 +0300

    Add stub for cafe repository function espresso_version() (#1257)

---
+++ stubs/cafe.php
@@ -0,0 +1,14 @@
+<?php
+
+// here I will include stubs related to Cafe repository for IDE auto-completion
+
+/**
+ * espresso_version
+ * Returns the plugin version
+ *
+ * @return string
+ */
+function espresso_version(): string
+{
+  return '';
+}
