a314ac4cabe41fc94713ebeba49ddce00a383913
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-09-20 00:34:49 +0300

    Add JSDoc comment for GraphQL entity types

--- domains/core/admin/blocks/src/services/apollo/types.ts
+++ domains/core/admin/blocks/src/services/apollo/types.ts
@@ -8,6 +8,13 @@ import type {
 	TicketsList as TicketList,
 } from '@eventespresso/data';
 
+/**
+ * Properties of GraphQL times i.e. types which extend Entity type are taken from EE Core
+ * There is simply too many types (included nested) so at any given point in time these types are *not* comprehensive
+ * So if properties for certain GraphQL type is missing, it is necessary to update the types here
+ * @see plugins/event-espresso-core/core/domain/services/graphql/types
+ */
+
 export interface Attendee extends Entity, Address {
 	avatar?: string;
 	bio?: string;
