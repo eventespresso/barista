6498fc8ee4df2823515409b81886d7f3653bea3e
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-09-21 01:57:37 +0300

    Add comment to Contexts.ts

--- packages/ui-components/src/EntityPaperFrame/Contexts.ts
+++ packages/ui-components/src/EntityPaperFrame/Contexts.ts
@@ -1,5 +1,10 @@
 import { createContext } from 'react';
 
+/**
+ * There are no official rules or guidelines telling that empty value is an invalid value
+ * @link https://stackoverflow.com/a/58058443/4343719
+ */
+
 const AriaLabelContext = createContext('');
 
 const AriaDescriptionContext = createContext('');
