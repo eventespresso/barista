b1515d20654531e049cf2658c7b2eed04b2a0b08
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-09-22 23:38:26 +0300

    Add aria-label for ticket assignment modal

--- domains/core/admin/eventEditor/src/ui/ticketAssignmentsManager/components/TicketAssignmentsManagerModal.tsx
+++ domains/core/admin/eventEditor/src/ui/ticketAssignmentsManager/components/TicketAssignmentsManagerModal.tsx
@@ -1,11 +1,14 @@
+import { useMemo, useContext } from 'react';
 import { __ } from '@eventespresso/i18n';
 import { CollapsibleLegend, ModalWithAlert } from '@eventespresso/ui-components';
+import type { ModalWithAlertProps } from '@eventespresso/ui-components';
 
 import { legendConfig } from './config';
 import TicketAssignmentsManager from './TicketAssignmentsManager';
 import { useDataState } from '../data';
 import type { TAMModalProps } from '../context/types';
 import useSubmitButtonProps from './useSubmitButtonProps';
+import { Context } from '../context/ContextProvider';
 
 import './styles.scss';
 
@@ -14,6 +17,20 @@ const TicketAssignmentsManagerModal: React.FC<Partial<TAMModalProps>> = ({ onClo
 	const hasErrors = hasOrphanEntities();
 
 	const submitButtonProps = useSubmitButtonProps(onSubmit);
+
+	const data = useContext(Context);
+
+	const ariaLabel: string = useMemo(() => {
+		if (!data.entity) {
+			return '<loading aria label>';
+		}
+		return `ticket assignment modal for ticket ${data.entity.name}`;
+	}, [data]);
+
+	const ariaAttributes: ModalWithAlertProps['ariaAttributes'] = useMemo(
+		() => ({ modalContent: { 'aria-label': ariaLabel } }),
+		[ariaLabel]
+	);
 
 	return (
 		<ModalWithAlert
@@ -26,6 +43,7 @@ const TicketAssignmentsManagerModal: React.FC<Partial<TAMModalProps>> = ({ onClo
 			submitButtonProps={submitButtonProps}
 			showAlertOnClose={isDirty || hasErrors}
 			title={title || __('Ticket Assignment Manager')}
+			ariaAttributes={ariaAttributes}
 		>
 			<TicketAssignmentsManager />
 			<CollapsibleLegend direction='row' legendConfig={legendConfig} />
