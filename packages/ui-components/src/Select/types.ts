3a6f74d89e8fedf8f6010ad531459bab3486773f
tn3rb <brent@eventespresso.com>
2023-10-03 20:08:09 -0700

    add width prop and class to Select

--- packages/ui-components/src/Select/types.ts
+++ packages/ui-components/src/Select/types.ts
@@ -1,6 +1,7 @@
 import type { SelectProps as AdapterSelectProps } from '@eventespresso/adapters';
+import type { Size, Width } from '../types';
 
-export interface SelectProps extends AdapterSelectProps {
+export interface SelectProps extends Omit<AdapterSelectProps, 'size' | 'width'>, Size, Width {
 	fitContainer?: boolean;
 	flow?: 'inline';
 	noBorderColor?: boolean;
