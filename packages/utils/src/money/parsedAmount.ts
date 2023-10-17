8c8251ac91060846dbf0079381a8a8e10c1f102f
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-10-17 03:33:25 +0300

    Update function parsedAmount

--- packages/utils/src/money/parsedAmount.ts
+++ packages/utils/src/money/parsedAmount.ts
@@ -6,15 +6,16 @@ const NON_NUMERIC_REGEX = /[^\d.-]+/g;
  * returns amount parsed as a float (if not already a number)
  *
  * @param {number|string} amount
+ * @param {boolean} what value to return if number cannot be parsed i.e. NaN condition
  * @return {number}
  */
-export const parsedAmount = (amount: Amount): number => {
+export const parsedAmount = (amount: Amount, fallback: number = 0): number => {
 	if (typeof amount === 'number') {
 		return amount;
 	}
-	const float = amount.replace(NON_NUMERIC_REGEX, '') as unknown;
-	if (Number.isNaN(float)) {
-		 return 0;
+	const parsedStr = amount.replace(NON_NUMERIC_REGEX, '');
+	if (Number.isNaN(parsedStr)) {
+		return fallback;
 	}
-	return float as number;
+	return Number.parseFloat(parsedStr);
 };
