6a2a44e1e9cff883cbef3b45bfa79d91139db3de
tn3rb <brent@eventespresso.com>
2023-11-29 16:29:48 -0800

    add new DatetimeStatus component to DateCard

--- domains/core/admin/eventEditor/src/ui/datetimes/datesList/cardView/DateCardSidebar.tsx
+++ domains/core/admin/eventEditor/src/ui/datetimes/datesList/cardView/DateCardSidebar.tsx
@@ -2,12 +2,14 @@ import { useCallback, useMemo } from 'react';
 
 import { __ } from '@eventespresso/i18n';
 import { CalendarDateSwitcher, EditDateRangeButton } from '@eventespresso/ee-components';
-import { getDatetimeStatusTextLabel } from '@eventespresso/helpers';
+import { DATETIME_STATUSES } from '@eventespresso/predicates';
 import { useDatesListFilterState } from '@eventespresso/edtr-services';
 import { useDatetimeMutator } from '@eventespresso/edtr-services';
+import { isActive, isExpired, isUpcoming } from '@eventespresso/predicates';
 import { useTimeZoneTime } from '@eventespresso/services';
-import type { DateRange } from '@eventespresso/dates';
+import { DatetimeStatus } from './DatetimeStatus';
 
+import type { DateRange } from '@eventespresso/dates';
 import type { DateItemProps } from '../types';
 
 const DateCardSidebar: React.FC<DateItemProps> = ({ entity: date }) => {
@@ -24,7 +26,29 @@ const DateCardSidebar: React.FC<DateItemProps> = ({ entity: date }) => {
 		},
 		[siteTimeToUtc, updateEntity]
 	);
-	const statusText = getDatetimeStatusTextLabel(date);
+
+	const onStatusChange = useCallback(
+		(status: keyof typeof DATETIME_STATUSES): void => {
+			let newStatus = status;
+
+			// convert "CC" (Calendar Controlled) status to upcoming, active, or expired status codes
+			if (newStatus === 'CC') {
+				if (isUpcoming(date, true)) {
+					newStatus = 'DTU';
+				}
+				if (isActive(date, true)) {
+					newStatus = 'DTA';
+				}
+				if (isExpired(date, true)) {
+					newStatus = 'DTE';
+				}
+			}
+			if (newStatus !== date.status) {
+				updateEntity({ status: DATETIME_STATUSES[newStatus] });
+			}
+		},
+		[date, updateEntity]
+	);
 
 	const labels = useMemo(() => {
 		return {
@@ -49,7 +73,7 @@ const DateCardSidebar: React.FC<DateItemProps> = ({ entity: date }) => {
 				startDate={date.startDate}
 				tooltip={__('edit start and end dates')}
 			/>
-			<div className='ee-entity-status-label'>{statusText}</div>
+			<DatetimeStatus date={date} onStatusChange={onStatusChange} />
 		</>
 	) : null;
 };
