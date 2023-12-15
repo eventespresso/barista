4796c1e22e23b14b462fd0c0426a1cd354ac6083
tn3rb <brent@eventespresso.com>
2023-12-15 09:22:08 -0800

    Add additional options to MAPS_TO_OPTIONS constant

--- domains/core/admin/eventEditor/src/ui/registrationForm/constants.ts
+++ domains/core/admin/eventEditor/src/ui/registrationForm/constants.ts
@@ -40,7 +40,35 @@ export const MAPS_TO_OPTIONS: OptionsType = [
 		label: __('Attendee Email Address'),
 	},
 	{
+		value: 'Attendee.email_confirm',
+		label: __('Attendee Email Address Confirmation'),
+	},
+	{
 		value: 'Attendee.address',
 		label: __('Attendee Address'),
+	},
+	{
+		value: 'Attendee.address2',
+		label: __('Attendee Address 2'),
+	},
+	{
+		value: 'Attendee.city',
+		label: __('Attendee City'),
+	},
+	{
+		value: 'Attendee.state',
+		label: __('Attendee State/Province'),
+	},
+	{
+		value: 'Attendee.country',
+		label: __('Attendee Country'),
+	},
+	{
+		value: 'Attendee.zip',
+		label: __('Attendee Zip/Postal Code'),
+	},
+	{
+		value: 'Attendee.phone',
+		label: __('Attendee Phone Number'),
 	},
 ];
