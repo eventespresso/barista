44c83e7ee57e8631f7f84fc42c140cce81aa0e83
tn3rb <brent@eventespresso.com>
2024-01-03 14:10:54 -0800

    Add useConfig import and update sanitizeAmount function

--- packages/utils/src/money/sanitizeAmount.ts
+++ packages/utils/src/money/sanitizeAmount.ts
@@ -1,4 +1,5 @@
 // import type { Amount } from './types';
+import { useConfig } from '@eventespresso/services';
 
 /**
  * returns amount parsed as a float (if not already a number)
@@ -7,8 +8,10 @@
  * @return {number}
  */
 export const sanitizeAmount = (amount: number | string): string => {
+	const { currency } = useConfig();
+	const decimalMark = currency?.decimalMark || '.';
 	let moneyValue: string = typeof amount === 'number' ? amount.toString() : amount;
-	const regex = /[^0-9.]/g;
+	const regex = /[^0-9${decimalMark}]/g;
 	moneyValue = moneyValue?.replace(regex, '');
 	console.log('%c sanitizeAmount()', 'color: SkyBlue; font-size: 12px;');
 	console.log('%c amount', 'color: SkyBlue;', amount);
