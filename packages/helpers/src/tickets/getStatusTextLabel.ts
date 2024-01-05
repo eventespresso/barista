606e4735ffa7336e9444a0dd5c169e4d94ca8de3
tn3rb <brent@eventespresso.com>
2024-01-04 18:01:37 -0800

    Refactor getStatusTextLabel function to use TICKET_STATUSES enum

--- packages/helpers/src/tickets/getStatusTextLabel.ts
+++ packages/helpers/src/tickets/getStatusTextLabel.ts
@@ -1,25 +1,24 @@
 import { __ } from '@eventespresso/i18n';
 
-import status from './status';
 import type { Ticket } from '@eventespresso/edtr-services';
-import { TICKET_STATUS_ID } from '@eventespresso/predicates';
+import { TICKET_STATUSES } from '@eventespresso/predicates';
 
 const getStatusTextLabel = (ticket: Ticket): string => {
 	let ticketStatus = '';
-	switch (status(ticket)) {
-		case TICKET_STATUS_ID.TRASHED:
+	switch (ticket.status) {
+		case TICKET_STATUSES.TRASHED:
 			ticketStatus = __('trashed');
 			break;
-		case TICKET_STATUS_ID.EXPIRED:
+		case TICKET_STATUSES.EXPIRED:
 			ticketStatus = __('expired');
 			break;
-		case TICKET_STATUS_ID.SOLD_OUT:
+		case TICKET_STATUSES.SOLD_OUT:
 			ticketStatus = __('sold out');
 			break;
-		case TICKET_STATUS_ID.PENDING:
+		case TICKET_STATUSES.PENDING:
 			ticketStatus = __('pending');
 			break;
-		case TICKET_STATUS_ID.ONSALE:
+		case TICKET_STATUSES.ONSALE:
 			ticketStatus = __('on sale');
 			break;
 	}
