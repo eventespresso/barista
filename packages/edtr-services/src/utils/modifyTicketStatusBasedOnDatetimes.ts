317295adc12946accc8d8be218050471bca0a89b
tn3rb <brent@eventespresso.com>
2024-01-15 11:18:04 -0800

    Refactor modifyTicketStatusBasedOnDatetimes function

--- packages/edtr-services/src/utils/modifyTicketStatusBasedOnDatetimes.ts
+++ packages/edtr-services/src/utils/modifyTicketStatusBasedOnDatetimes.ts
@@ -10,60 +10,109 @@ export const modifyTicketStatusBasedOnDatetimes = (ticket: Ticket) => {
 	const { getRelations } = useRelations();
 	const relatedDatetimeIds = getRelations({ entity: 'tickets', entityId: ticket.id, relation: 'datetimes' });
 
-	let changes = {};
+	let newStatus = '';
 	for (const relatedDatetimeId of relatedDatetimeIds) {
 		const datetime = findDatetime(relatedDatetimeId);
 		if (datetime.id) {
 			switch (datetime.status) {
-				// if at least one datetime is active or upcoming, then just use ticket status
+				case 'SOLD_OUT':
+					// if any datetime is sold out, then the ticket needs to be sold out too
+					newStatus = TICKET_STATUSES.SOLD_OUT;
+					break;
 				case 'ACTIVE':
 				case 'UPCOMING':
-					return ticket;
+					// unless ticket is sold out, then it should be on sale
+					if (newStatus !== TICKET_STATUSES.SOLD_OUT) {
+						newStatus = TICKET_STATUSES.ONSALE;
+					}
+					break;
 				case 'POSTPONED':
 				case 'TO_BE_DETERMINED':
-					changes = {
-						isExpired: false,
-						isOnSale: false,
-						isPending: true,
-						isSoldOut: false,
-						isTrashed: false,
-						status: TICKET_STATUSES.PENDING,
-					};
+					// if ticket is not sold out and not on sale, then it mark it pending
+					if (newStatus !== TICKET_STATUSES.ONSALE && newStatus !== TICKET_STATUSES.SOLD_OUT) {
+						newStatus = TICKET_STATUSES.PENDING;
+					}
+					break;
+				case 'EXPIRED':
+					// only change to expired if status is not one of the above statuses
+					if (
+						newStatus !== TICKET_STATUSES.SOLD_OUT &&
+						newStatus !== TICKET_STATUSES.ONSALE &&
+						newStatus !== TICKET_STATUSES.PENDING
+					) {
+						newStatus = TICKET_STATUSES.EXPIRED;
+					}
 					break;
 				case 'CANCELLED':
 				case 'TRASHED':
-					changes = {
-						isExpired: false,
-						isOnSale: false,
-						isPending: false,
-						isSoldOut: false,
-						isTrashed: true,
-						status: TICKET_STATUSES.TRASHED,
-					};
-					break;
-				case 'EXPIRED':
-					changes = {
-						isExpired: true,
-						isOnSale: false,
-						isPending: false,
-						isSoldOut: false,
-						isTrashed: false,
-						status: TICKET_STATUSES.EXPIRED,
-					};
-					break;
-				case 'SOLD_OUT':
-					changes = {
-						isExpired: false,
-						isOnSale: false,
-						isPending: false,
-						isSoldOut: true,
-						isTrashed: false,
-						status: TICKET_STATUSES.SOLD_OUT,
-					};
+					// only change to trashed if status has not been changed to something else
+					if (newStatus === '') {
+						newStatus = TICKET_STATUSES.TRASHED;
+					}
 					break;
 			}
 		}
 	}
-	// but if the ticket status is not active or upcoming, then change the ticket status
-	return { ...ticket, ...changes };;
+	if (newStatus !== ticket.status) {
+		switch (newStatus) {
+			case TICKET_STATUSES.PENDING:
+				return changeTicketStatusToPending(ticket);
+			case TICKET_STATUSES.TRASHED:
+				return changeTicketStatusToTrashed(ticket);
+			case TICKET_STATUSES.EXPIRED:
+				return changeTicketStatusToExpired(ticket);
+			case TICKET_STATUSES.SOLD_OUT:
+				return changeTicketStatusToSoldOut(ticket);
+		}
+	}
+	return ticket;
 };
+
+
+export const changeTicketStatusToPending = (ticket: Ticket) => {
+	const changes = {
+		isExpired: false,
+		isOnSale: false,
+		isPending: true,
+		isSoldOut: false,
+		isTrashed: false,
+		status: TICKET_STATUSES.PENDING,
+	};
+	return { ...ticket, ...changes };
+}
+
+export const changeTicketStatusToTrashed = (ticket: Ticket) => {
+	const changes = {
+		isExpired: false,
+		isOnSale: false,
+		isPending: false,
+		isSoldOut: false,
+		isTrashed: true,
+		status: TICKET_STATUSES.TRASHED,
+	};
+	return { ...ticket, ...changes };
+}
+
+export const changeTicketStatusToExpired = (ticket: Ticket) => {
+	const changes = {
+		isExpired: true,
+		isOnSale: false,
+		isPending: false,
+		isSoldOut: false,
+		isTrashed: false,
+		status: TICKET_STATUSES.EXPIRED,
+	};
+	return { ...ticket, ...changes };
+}
+
+export const changeTicketStatusToSoldOut = (ticket: Ticket) => {
+	const changes = {
+		isExpired: false,
+		isOnSale: false,
+		isPending: false,
+		isSoldOut: true,
+		isTrashed: false,
+		status: TICKET_STATUSES.SOLD_OUT,
+	};
+	return { ...ticket, ...changes };
+}
