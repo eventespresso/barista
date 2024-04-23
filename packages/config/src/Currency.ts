4d024366bd38648a2777371cc9ad6ed52c087cdc
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2024-04-23 17:56:16 +0300

    Refactor Currency.ts

--- packages/config/src/Currency.ts
+++ packages/config/src/Currency.ts
@@ -1,15 +1,19 @@
 import type { CurrencyProps } from './types';
 
-export const Currency = (config: CurrencyProps): CurrencyProps => {
+export const Currency = (config?: Partial<CurrencyProps>): CurrencyProps => {
 	return {
-		code: config?.code || 'USD',
-		singularLabel: config?.singularLabel || 'Dollar',
-		pluralLabel: config?.pluralLabel || 'Dollars',
-		sign: config?.sign || '$',
-		signB4: config?.signB4,
-		decimalPlaces: config?.decimalPlaces >= 0 ? config.decimalPlaces : 2,
-		decimalMark: config?.decimalMark || '.',
-		thousandsSeparator: config?.thousandsSeparator || ',',
-		subunits: config?.subunits >= 0 ? config.subunits : Math.pow(10, config?.decimalPlaces || 2),
+		...defaultCurrency,
+		...config,
 	};
+};
+
+const defaultCurrency: CurrencyProps = {
+	code: 'USD',
+	singularLabel: 'Dollar',
+	pluralLabel: 'Dollars',
+	sign: '$',
+	signB4: false,
+	decimalPlaces: 2,
+	decimalMark: '.',
+	thousandsSeparator: ',',
 };
