62f5d28d9660c4193bffdb0d205ee7886b260a19
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2024-05-06 22:59:27 +0300

    Move component FormattedInput under directory "input"

--- packages/tpc/src/components/price/formatted-price/index.ts
+++
@@ -1,1 +0,0 @@
-export { FormattedPrice } from './FormattedPrice';
--- packages/tpc/src/components/price/index.ts
+++ packages/tpc/src/components/price/index.ts
@@ -2,5 +2,4 @@ export * from './types';
 export { BaseField } from './base-field';
 export { PriceField } from './price-field';
 export { MoneyInputWithConfig } from './money-config';
-export { FormattedPrice } from './formatted-price';
 export * as Input from './input';
