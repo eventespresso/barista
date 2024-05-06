ec9f6c488e1462813597a85574110131bf599e87
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2024-05-06 22:52:10 +0300

    Add sub-component Type to "amount"

---
+++ packages/tpc/src/components/price/input/Type.tsx
@@ -0,0 +1,36 @@
+import { __ } from '@eventespresso/i18n';
+import { usePriceTypes } from '@eventespresso/edtr-services';
+import { getPriceModifiers } from '@eventespresso/predicates';
+
+import { useDataState } from '../../../data';
+import { PriceField } from '..';
+
+import type { PriceModifierProps } from '../../../types';
+
+export const Type: React.FC<PriceModifierProps> = ({ price }) => {
+	const { isDisabled } = useDataState();
+	const priceTypes = usePriceTypes();
+	const modifierOptions = getPriceModifiers(priceTypes);
+	const options = price.isBasePrice ? priceTypes : modifierOptions;
+	const className = !options.length ? 'ee-select--error' : null;
+	// price type cannot be changed for base/default price
+	const disabled = isDisabled || price.isBasePrice || price.isDefault;
+
+	return (
+		<PriceField
+			aria-label={__('price type')}
+			className={className}
+			component={'select'}
+			disabled={disabled}
+			field='priceType'
+			price={price}
+			type='select'
+		>
+			{options.map((option) => (
+				<option key={option.id} value={option.id}>
+					{option.name}
+				</option>
+			))}
+		</PriceField>
+	);
+};
