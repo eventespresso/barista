d9c7ad1c6fa2030419c3fa4d412cefe7c98d82a1
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2024-05-06 22:52:39 +0300

    Export components from directory "price"

--- packages/tpc/src/components/index.ts
+++ packages/tpc/src/components/index.ts
@@ -1,2 +1,4 @@
 export { default as TicketPriceCalculator } from './TicketPriceCalculator';
 export { default as ModalContainer } from './ModalContainer';
+export * as Price from './price'; // for types and components
+export * as price from './price'; // for hooks
