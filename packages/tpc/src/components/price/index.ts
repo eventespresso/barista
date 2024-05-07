6b4967abcba70ea77907396ef068bf8ccb575692
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2024-05-07 21:21:53 +0300

    Remove unused component BaseNumberInputField.tsx

--- packages/tpc/src/components/price/index.ts
+++ packages/tpc/src/components/price/index.ts
@@ -1,5 +1,4 @@
 export * from './types';
 export { BaseField, useBaseField } from './base-field';
-export { BaseNumberInputField } from './base-number';
 export { MoneyInputWithConfig } from './money-config';
 export * as Input from './input';
