6917d65da3139ccbe5da85e15b5fcee19d0613e0
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2024-05-10 19:01:41 +0300

    Move factory to its own directory

--- packages/tpc/src/components/price/index.ts
+++ packages/tpc/src/components/price/index.ts
@@ -1,3 +1,4 @@
 export * from './types';
-export { BaseField, useBaseField } from './base-field';
+export { BaseField, useBaseField } from './base-field'; // TODO: remove
+export { Factory } from './factory/Factory';
 export * as Input from './input';
