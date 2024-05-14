35c04c89fb256bad7673b1953ee0eb0de1118529
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2024-05-14 18:22:12 +0300

    Remove folder "base-field" and its components/hooks

--- packages/tpc/src/components/price/base-field/index.ts
+++
@@ -1,2 +0,0 @@
-export { BaseField } from './BaseField';
-export { useBaseField } from './useBaseField';
--- packages/tpc/src/components/price/index.ts
+++ packages/tpc/src/components/price/index.ts
@@ -1,4 +1,3 @@
 export * from './types';
-export { BaseField, useBaseField } from './base-field'; // TODO: remove
 export { Factory } from './factory/Factory';
 export * as Input from './input';
