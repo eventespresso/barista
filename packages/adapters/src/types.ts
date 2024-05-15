688b36bf1ee6c5ee6dc4ce30a8fecbe74a55410f
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2024-05-15 21:39:27 +0300

    Narrow prop types for package "adapters"

--- packages/adapters/src/types.ts
+++ packages/adapters/src/types.ts
@@ -1,6 +1,8 @@
-export type CommonInputEvent<T = Element> = React.ChangeEvent<T> | React.FormEvent<T>;
+import type React from 'react';
 
-export interface CommonInputProps<T = Element, V = React.ReactText> {
+export type CommonInputEvent<T extends HTMLElement = HTMLElement> = React.ChangeEvent<T> | React.FormEvent<T>;
+
+export interface CommonInputProps<T extends HTMLElement = HTMLElement, V extends React.ReactText = React.ReactText> {
 	onChange?: (valueAsString: string, valueAsNumber: number) => void | React.ChangeEventHandler<HTMLInputElement>;
 	onChangeValue?: ((value: V, event?: CommonInputEvent<T>) => void) | ((value: any) => void);
 }
