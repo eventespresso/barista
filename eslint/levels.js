b9b24ed1da3e5f08426dc614fa9c230e94a98035
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2024-05-15 19:02:27 +0300

    Lower dependency level for "services" package

--- eslint/levels.js
+++ eslint/levels.js
@@ -1,13 +1,13 @@
 module.exports = [
-	/* LEVEL -1 */ ['types'],
-	/* LEVEL 0 */ ['config'],
+	/* LEVEL B1 */ ['types'],
+	/* LEVEL G */ ['config'],
 	/* LEVEL 1 */ ['constants', 'i18n', 'events', 'ioc', 'toaster', 'utils'],
 	/* LEVEL 2 */ ['data', 'dates', 'hooks', 'icons', 'plugins', 'slot-fill', 'registry', 'styles', 'storage'],
 	/* LEVEL 3 */ ['adapters', 'predicates'],
 	/* LEVEL 4 */ ['ui-components'],
+	/* LEVEL 7 */ ['services'],
 	/* LEVEL 5 */ ['rich-text-editor'],
 	/* LEVEL 6 */ ['form', 'form-builder', 'rrule-generator'],
-	/* LEVEL 7 */ ['services'],
 	/* LEVEL 8 */ ['edtr-services', 'ee-components', 'helpers'],
 	/* LEVEL 9 */ ['tpc'],
 ];
