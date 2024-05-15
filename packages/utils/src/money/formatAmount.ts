4eb07897cb1e27e4b17d84936096e6a748f09f1b
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2024-05-15 22:09:28 +0300

    Update comment for function formatAmount()

--- packages/utils/src/money/formatAmount.ts
+++ packages/utils/src/money/formatAmount.ts
@@ -5,11 +5,7 @@ import type { Amount } from './types';
 export type FormatAmountFunction = (amount: Amount) => string;
 
 /**
- * returns a function that when supplied a value for the number of decimal places used by a currency,
- * returns a second function that can be passed an amount which will then be appropriately formatted
- *
- * @param {number} decimalPlaces
- * @return {Function}
+ * Function factory that formats the given input according the number of given decimal places
  */
 export const formatAmount =
 	(decimalPlaces: number): FormatAmountFunction =>
