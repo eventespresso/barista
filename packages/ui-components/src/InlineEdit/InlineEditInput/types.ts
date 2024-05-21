9922c887ed0e287d49849fce3a8ac311d197f9f5
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2024-05-22 02:33:19 +0300

    Refactor types.ts (another file)

--- packages/ui-components/src/InlineEdit/InlineEditInput/types.ts
+++ packages/ui-components/src/InlineEdit/InlineEditInput/types.ts
@@ -1,47 +1,19 @@
-import type * as Chakra from '@chakra-ui/react';
-import { CommonInputProps } from '@eventespresso/adapters';
+import { InlineEditType as InlineEdit } from '@eventespresso/adapters';
 
-export type InlineEditProps = InlineEditAdapterProps &
-	Partial<{
-		tag: React.ElementType;
-	}>;
+export type InlineEditProps = InlineEdit.Component.Props & {
+	tag?: React.ElementType;
+};
 
-export type TextareaProps = InlineEditProps &
-	Partial<{
-		richTextContent: boolean;
-	}>;
+export type TextareaProps = InlineEditProps & {
+	richTextContent?: boolean;
+};
 
-export type PreviewProps = Partial<InlineEditPreviewProps> &
-	Partial<{
-		className: string;
-		lineCount: number;
-		lineLength: number;
-		isEditing: boolean;
-		onRequestEdit: VoidFunction;
-		richTextContent: boolean;
-		tooltip: string;
-	}>;
-
-interface InlineEditAdapterProps extends ChakraProps, CommonProps {
-	editableInputClassName?: string;
-	inputClassName?: string;
+export type PreviewProps = InlineEdit.Preview.Props & {
+	className?: string;
 	lineCount?: number;
-	Preview?: React.ComponentType<InlineEditPreviewProps>;
-	previewClassName?: string;
-	textAreaClassName?: string;
-	tooltip?: string;
-}
-
-type CommonProps = Omit<CommonInputProps<HTMLInputElement>, 'onChange' | 'onChangeValue'>;
-
-type ChakraProps = Partial<Chakra.EditableProps>;
-
-interface InlineEditPreviewProps extends InlineEditBase {
+	lineLength?: number;
 	isEditing?: boolean;
-	onRequestEdit?: () => void;
-	value?: string;
-}
-
-type InlineEditBase = Partial<
-	Omit<InlineEditAdapterProps, 'onCancel' | 'onChange' | 'onChangeValue' | 'onEdit' | 'onSubmit'>
->;
+	onRequestEdit?: VoidFunction;
+	richTextContent?: boolean;
+	tooltip?: string;
+};
