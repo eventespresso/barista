5b55b61d53dc05e7aafe9f467c23d03e7a9e3e8e
Brent Christensen <brent@eventespresso.com>
2024-05-21 11:53:33 -0700

    Add ItemCount to Datetime Registrations Link (#1309)

--- domains/core/admin/eventEditor/src/ui/datetimes/datesList/DateRegistrationsLink.tsx
+++ domains/core/admin/eventEditor/src/ui/datetimes/datesList/DateRegistrationsLink.tsx
@@ -1,6 +1,6 @@
 import { __ } from '@eventespresso/i18n';
-import { RegistrationsLink } from '@eventespresso/ui-components';
-import { useRegistrationsLink } from '@eventespresso/edtr-services';
+import { RegistrationsLink, ItemCount } from '@eventespresso/ui-components';
+import { useRegistrationsLink, useRelatedTickets } from '@eventespresso/edtr-services';
 import type { Datetime } from '@eventespresso/edtr-services';
 
 interface Props {
@@ -8,9 +8,26 @@ interface Props {
 }
 
 export const DateRegistrationsLink: React.FC<Props> = ({ datetime }) => {
+	const getRelatedTickets = useRelatedTickets();
+
+	const tickets = getRelatedTickets({
+		entity: 'datetimes',
+		entityId: datetime.id,
+	});
+
+	let count = 0;
+	for (const ticket of tickets) {
+		count += ticket.registrationCount;
+	}
+
 	const regListUrl = useRegistrationsLink({ datetime_id: datetime.dbId });
 
+	const countTitle = __('total registrations.');
 	const tooltip = __('view ALL registrations for this date.');
 
-	return <RegistrationsLink href={regListUrl} tooltip={tooltip} />;
+	return (
+		<ItemCount count={count} emphasizeZero={false} title={countTitle}>
+			<RegistrationsLink href={regListUrl} tooltip={tooltip} />
+		</ItemCount>
+	);
 };
