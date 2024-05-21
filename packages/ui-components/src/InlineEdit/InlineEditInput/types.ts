1ef68d35048ab6103d357a24dbc4a2b2393360f5
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2024-05-22 01:36:08 +0300

    Reduce external type-related dependencies in types.ts

--- packages/ui-components/src/InlineEdit/InlineEditInput/types.ts
+++ packages/ui-components/src/InlineEdit/InlineEditInput/types.ts
@@ -1,23 +1,47 @@
-import { InlineEditProps as InlineEditAdapterProps, InlineEditPreviewProps } from '@eventespresso/adapters';
+import type * as Chakra from '@chakra-ui/react';
+import { CommonInputProps } from '@eventespresso/adapters';
 
-export interface InlineEditProps extends Omit<InlineEditAdapterProps, 'inputType'> {
+export type InlineEditProps = InlineEditAdapterProps &
+	Partial<{
+		tag: React.ElementType;
+	}>;
+
+export type TextareaProps = InlineEditProps &
+	Partial<{
+		richTextContent: boolean;
+	}>;
+
+export type PreviewProps = Partial<InlineEditPreviewProps> &
+	Partial<{
+		className: string;
+		lineCount: number;
+		lineLength: number;
+		isEditing: boolean;
+		onRequestEdit: VoidFunction;
+		richTextContent: boolean;
+		tooltip: string;
+	}>;
+
+interface InlineEditAdapterProps extends ChakraProps, CommonProps {
+	editableInputClassName?: string;
+	inputClassName?: string;
 	lineCount?: number;
-	tag?: React.ElementType;
+	Preview?: React.ComponentType<InlineEditPreviewProps>;
+	previewClassName?: string;
+	textAreaClassName?: string;
 	tooltip?: string;
 }
 
-export interface TextareaProps extends Omit<InlineEditProps, 'inputType'> {
-	lineCount?: number;
-	richTextContent?: boolean;
-	tooltip?: string;
-}
+type CommonProps = Omit<CommonInputProps<HTMLInputElement>, 'onChange' | 'onChangeValue'>;
 
-export interface PreviewProps extends Partial<InlineEditPreviewProps> {
-	className?: string;
-	lineCount?: number;
-	lineLength?: number;
+type ChakraProps = Partial<Chakra.EditableProps>;
+
+interface InlineEditPreviewProps extends InlineEditBase {
 	isEditing?: boolean;
-	onRequestEdit?: VoidFunction;
-	richTextContent?: boolean;
-	tooltip?: string;
+	onRequestEdit?: () => void;
+	value?: string;
 }
+
+type InlineEditBase = Partial<
+	Omit<InlineEditAdapterProps, 'onCancel' | 'onChange' | 'onChangeValue' | 'onEdit' | 'onSubmit'>
+>;
