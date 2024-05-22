38f22be303c957859414fa83a460b1ff93ff5a3a
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2024-05-22 23:51:02 +0300

    Update types.ts

--- packages/adapters/src/InlineEdit/types.ts
+++ packages/adapters/src/InlineEdit/types.ts
@@ -4,28 +4,19 @@ import type { EditableInputProps, EditablePreviewProps, UseEditableReturn } from
 import type { TooltipProps } from '../Tooltip';
 
 export module Component {
-	export type Props = Chakra.EditableProps & {
-		editableInputClassName?: string;
-		inputClassName?: string;
-		inputType?: 'heading' | 'number' | 'textarea' | 'text';
-		lineCount?: number;
-		Preview?: React.ComponentType<Preview.Props>;
-		previewClassName?: string;
-		textAreaClassName?: string;
-		tooltip?: string;
-	};
-
-	export type NewProps = {
-		container?: Chakra.EditableProps;
-
-		preview?: Preview.Props & {
-			component?: Preview;
-		};
-
+	export type Props = {
+		container?: Container.Props;
+		preview?: PreviewProps;
 		input?: Input.Props;
 	};
 
-	type Preview = React.ComponentType<Preview.Props>;
+	type PreviewProps = Preview.Props & {
+		component?: React.FunctionComponent<Preview.Props>;
+	};
+}
+
+export module Container {
+	export type Props = Chakra.EditableProps;
 }
 
 export module Preview {
@@ -39,9 +30,9 @@ export module Preview {
 
 export module Input {
 	export type Props = EditableInputProps & {
-		onCancel: UseEditableReturn['onCancel'];
-		setValue: React.Dispatch<React.SetStateAction<string>>;
-		type: 'heading' | 'number' | 'textarea' | 'text';
+		onCancel?: UseEditableReturn['onCancel'];
+		setValue?: React.Dispatch<React.SetStateAction<string>>;
+		type?: 'heading' | 'number' | 'textarea' | 'text';
 		// MAYBE: refactor
 		className?: {
 			input?: string;
