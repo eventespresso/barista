59173b7402f74e1cdc6add782cc131fcbf66725c
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2024-05-24 00:17:58 +0300

    Flatten directory structure

--- packages/adapters/src/InlineEdit/InlineEdit/index.ts
+++
@@ -1,5 +0,0 @@
-export * from '../types';
-export { InlineEdit as Component } from './InlineEdit';
-export { Adapter as InlineEdit } from './Adapter';
-export { Text } from './Text';
-export { Textarea } from './Textarea';
--- packages/adapters/src/InlineEdit/index.ts
+++ packages/adapters/src/InlineEdit/index.ts
@@ -1,2 +1,7 @@
-export { InlineEdit } from './InlineEdit';
-export * as InlineEditType from './types';
+export * from './types';
+
+export { InlineEdit as Component } from './InlineEdit';
+export { Adapter as InlineEdit } from './Adapter';
+
+export { Text } from './Text';
+export { Textarea } from './Textarea';
