004fbcbd1f6458de604c0126ee73d50790e9b296
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2024-05-26 05:21:26 +0300

    Create legacy-types.ts

--- packages/adapters/src/InlineEdit/index.ts
+++ packages/adapters/src/InlineEdit/index.ts
@@ -1,4 +1,5 @@
 export * as InlineEditType from './types';
+export type { Legacy } from './legacy-types';
 export { InlineEdit as Component } from './InlineEdit';
 export { Adapter as InlineEdit } from './Adapter';
 export { Textarea } from './Textarea';
---
+++ packages/adapters/src/InlineEdit/legacy-types.ts
@@ -0,0 +1,45 @@
+import type * as Chakra from '@chakra-ui/react';
+
+export module Legacy {
+	/**
+	 * @deprecated these are deprecated but to avoid HUGE amount of refactoring, legacy props will be used as an adapter between new and old types
+	 */
+	export type InputType = 'heading' | 'number' | 'text' | 'textarea';
+
+	/**
+	 * @deprecated these are deprecated but to avoid HUGE amount of refactoring, legacy props will be used as an adapter between new and old types
+	 */
+	export type InlineEditProps = Chakra.EditableProps & {
+		editableInputClassName?: string;
+		inputClassName?: string;
+		inputType?: InputType;
+		lineCount?: number;
+		Preview?: React.ComponentType<InlineEditPreviewProps>;
+		previewClassName?: string;
+		textAreaClassName?: string;
+		tooltip?: string;
+	};
+
+	/**
+	 * @deprecated these are deprecated but to avoid HUGE amount of refactoring, legacy props will be used as an adapter between new and old types
+	 */
+	export type InlineEditPreviewProps = Omit<
+		InlineEditProps,
+		'onCancel' | 'onChange' | 'onChangeValue' | 'onEdit' | 'onSubmit'
+	> & {
+		isEditing?: boolean;
+		onRequestEdit?: () => void;
+		value?: string;
+	};
+
+	/**
+	 * @deprecated these are deprecated but to avoid HUGE amount of refactoring, legacy props will be used as an adapter between new and old types
+	 */
+	export type InlineEditInputProps = Pick<
+		InlineEditProps,
+		'editableInputClassName' | 'inputType' | 'textAreaClassName'
+	> & {
+		onCancel: () => void;
+		setValue: React.Dispatch<React.SetStateAction<string>>;
+	};
+}
