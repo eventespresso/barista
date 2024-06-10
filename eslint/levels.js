5e0d73648e1ce194e826730efa83031a91b3917b
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2024-06-10 23:49:46 +0300

    Update levels.js

--- eslint/levels.js
+++ eslint/levels.js
@@ -1,13 +1,14 @@
+// lower packages CANNOT import higher level packages
 module.exports = [
-	/* LEVEL B1 */ ['types'],
-	/* LEVEL G */ ['config'],
-	/* LEVEL 1 */ ['constants', 'i18n', 'events', 'ioc', 'toaster', 'utils'],
-	/* LEVEL 2 */ ['data', 'dates', 'hooks', 'icons', 'plugins', 'slot-fill', 'registry', 'styles', 'storage'],
-	/* LEVEL 3 */ ['adapters', 'predicates', 'services'],
-	/* LEVEL 4 */ ['ui-components'],
-	/* LEVEL 7 */ [],
-	/* LEVEL 5 */ ['rich-text-editor'],
-	/* LEVEL 6 */ ['form', 'form-builder', 'rrule-generator'],
-	/* LEVEL 8 */ ['edtr-services', 'ee-components', 'helpers'],
-	/* LEVEL 9 */ ['tpc'],
+	['types'],
+	['config'],
+	['constants', 'i18n', 'events', 'ioc', 'toaster', 'utils'],
+	['data', 'dates', 'hooks', 'icons', 'plugins', 'slot-fill', 'registry', 'styles', 'storage'],
+	['adapters', 'predicates', 'services'],
+	['ui-components'],
+	['rich-text-editor'],
+	['form', 'form-builder', 'rrule-generator'],
+	['edtr-services', 'ee-components', 'helpers'],
+	['tpc'],
 ];
+// higher packages CAN import lower level packages
