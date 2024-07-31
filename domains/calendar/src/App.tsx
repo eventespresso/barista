737f5d62c3b4287cf4849f8b04a7663af8eab985
mohsinsr <mohsinsabir.me@gmail.com>
2024-07-31 22:33:46 +0500

    unsuccessful try to connect with cafe calendar

--- domains/calendar/src/App.tsx
+++ domains/calendar/src/App.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 
 const App = () => {
+	document.addEventListener('DOMContentLoaded', function () {
+		const rootElement = document.getElementById('espresso-calendar');
+		rootElement.innerText = 'Hello';
+		// setInterval(() => {
+		// 	console.log('Im running');
+		// }, 1000);
+	});
+	React.useEffect(() => {
+		setInterval(() => {
+			console.log('Application is running');
+		}, 3000);
+	}, []);
+
 	return (
 		<React.Fragment>
 			<div>Hello World kkkkkk</div>
--- domains/calendar/src/index.tsx
+++ domains/calendar/src/index.tsx
@@ -3,4 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 // import './App.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('espresso-calendar'));
+
+// document.addEventListener('DOMContentLoaded', function () {
+// 	const rootElement = document.getElementById('espresso-calendar');
+// 	rootElement.innerText = 'Hello';
+// 	// setInterval(() => {
+// 	// 	console.log('Im running');
+// 	// }, 1000);
+// });
